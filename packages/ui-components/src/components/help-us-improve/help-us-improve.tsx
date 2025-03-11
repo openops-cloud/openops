0b2438cdd1ec8de05d444edf6c5e5b543b980d7d
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2025-03-11 16:27:35 +0200

    adapt remarks from FE channel

--- packages/ui-components/src/components/help-us-improve/help-us-improve.tsx
+++ packages/ui-components/src/components/help-us-improve/help-us-improve.tsx
@@ -1,7 +1,6 @@
 import { t } from 'i18next';
 import { cn } from '../../lib/cn';
 import { Button } from '../../ui/button';
-import { DismissiblePanel } from '../dismissible-panel/dismissible-panel';
 
 type Props = {
   className?: string;
@@ -10,35 +9,42 @@ type Props = {
 };
 
 const HelpUsImprove = ({ className, onDismiss, onAccept }: Props) => (
-  <DismissiblePanel
-    closeTooltip={t('Dismiss')}
-    onClose={onDismiss}
-    className={cn('p-2 bg-blueAccent/10 max-h-fit', className)}
+  <div
+    className={cn(
+      'p-3 bg-blueAccent/10 max-h-fit rounded-md border border-gray-200',
+      className,
+    )}
   >
-    <div className="p-1">
-      <p className="font-bold text-sm">
-        <span role="img" className="mx-2" aria-label="LightBulb">
-          💡
-        </span>
-        {t('Help Us Improve!')}
-      </p>
-      <p className="text-sm mx-2 mt-1">
-        {t(
-          'By sharing anonymous usage data, you enable us to refine and enhance your experience.',
-        )}
-      </p>
-      <div className="flex justify-end mt-[16px] mr-4">
-        <Button
-          variant="default"
-          size="sm"
-          className="font-bold h-7"
-          onClick={onAccept}
-        >
-          {t('Accept')}
-        </Button>
-      </div>
+    <p className="font-bold text-sm">
+      <span role="img" className="mx-2" aria-label="LightBulb">
+        💡
+      </span>
+      {t('Help Us Improve!')}
+    </p>
+    <p className="text-sm mx-2 mt-1">
+      {t(
+        'By sharing anonymous usage data, you enable us to refine and enhance your experience.',
+      )}
+    </p>
+    <div className="flex items-center justify-between mt-[16px] mr-3">
+      <Button
+        variant={'ghost'}
+        size="sm"
+        className="text-gray-500"
+        onClick={onDismiss}
+      >
+        {t('Close')}
+      </Button>
+      <Button
+        variant="default"
+        size="sm"
+        className="font-bold h-7"
+        onClick={onAccept}
+      >
+        {t('Accept')}
+      </Button>
     </div>
-  </DismissiblePanel>
+  </div>
 );
 
 HelpUsImprove.displayName = 'HelpUsImprove';
