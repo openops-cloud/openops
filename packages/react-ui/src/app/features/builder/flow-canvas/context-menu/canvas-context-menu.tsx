af5a5e42e26b4c5954b7f39de027f92b7814c04d
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2025-03-18 15:21:30 +0100

    extract canvas context menu

---
+++ packages/react-ui/src/app/features/builder/flow-canvas/context-menu/canvas-context-menu.tsx
@@ -0,0 +1,183 @@
+import {
+  Button,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  toast,
+  UNSAVED_CHANGES_TOAST,
+  WorkflowNode,
+} from '@openops/components/ui';
+import { FlowOperationType } from '@openops/shared';
+import {
+  DropdownMenu,
+  DropdownMenuSeparator,
+} from '@radix-ui/react-dropdown-menu';
+import { t } from 'i18next';
+import {
+  ArrowRightLeft,
+  Copy,
+  CopyPlus,
+  EllipsisVertical,
+  Trash,
+} from 'lucide-react';
+import { memo } from 'react';
+import { useBuilderStateContext } from '../../builder-hooks';
+import { useApplyOperationAndPushToHistory } from '../../flow-version-undo-redo/hooks/apply-operation-and-push-to-history';
+import { StepActionWrapper } from '../nodes/step-action-wrapper';
+
+type Props = {
+  data: WorkflowNode['data'];
+  isAction: boolean;
+  openStepActionsMenu: boolean;
+  setOpenStepActionsMenu: (openStepActionsMenu: boolean) => void;
+  setOpenBlockSelector: (openBlockSelector: boolean) => void;
+};
+
+const CanvasContextMenu = memo(
+  ({
+    data,
+    isAction,
+    openStepActionsMenu,
+    setOpenStepActionsMenu,
+    setOpenBlockSelector,
+  }: Props) => {
+    const applyOperationAndPushToHistory = useApplyOperationAndPushToHistory();
+
+    const [selectStepByName, removeStepSelection, setAllowCanvasPanning] =
+      useBuilderStateContext((state) => [
+        state.selectStepByName,
+        state.removeStepSelection,
+        state.setAllowCanvasPanning,
+      ]);
+
+    const deleteStep = () => {
+      applyOperationAndPushToHistory(
+        {
+          type: FlowOperationType.DELETE_ACTION,
+          request: {
+            name: data.step!.name,
+          },
+        },
+        () => toast(UNSAVED_CHANGES_TOAST),
+      );
+      removeStepSelection();
+    };
+
+    const duplicateStep = () =>
+      applyOperationAndPushToHistory(
+        {
+          type: FlowOperationType.DUPLICATE_ACTION,
+          request: {
+            stepName: data.step!.name,
+          },
+        },
+        () => toast(UNSAVED_CHANGES_TOAST),
+      );
+
+    return (
+      <div
+        onClick={(e) => {
+          e.stopPropagation();
+          e.preventDefault();
+        }}
+      >
+        <DropdownMenu
+          open={openStepActionsMenu}
+          onOpenChange={(open) => {
+            setOpenStepActionsMenu(open);
+          }}
+          modal={true}
+        >
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="ghost"
+              size="sm"
+              className="p-1 size-6"
+              onClick={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+              }}
+            >
+              <EllipsisVertical className="w-4 h-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="w-44 absolute"
+            onCloseAutoFocus={(e) => e.preventDefault()}
+          >
+            <DropdownMenuItem
+              onSelect={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+
+                setOpenStepActionsMenu(false);
+                setOpenBlockSelector(true);
+                selectStepByName(data.step!.name!);
+              }}
+            >
+              <StepActionWrapper>
+                <ArrowRightLeft className=" h-4 w-4 " />
+                <span>{t('Replace')} </span>
+              </StepActionWrapper>
+            </DropdownMenuItem>
+
+            {/* todo flag */}
+            {isAction && (
+              <>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem
+                  onSelect={(e) => {
+                    // implement copy logic
+                    // e.preventDefault();
+                  }}
+                >
+                  <StepActionWrapper>
+                    <Copy className="mr-2 h-4 w-4" />
+                    <span className="">{t('Copy')}</span>
+                  </StepActionWrapper>
+                </DropdownMenuItem>
+              </>
+            )}
+
+            {isAction && (
+              <DropdownMenuItem
+                onSelect={(e) => {
+                  e.preventDefault();
+                  duplicateStep();
+                  setOpenStepActionsMenu(false);
+                }}
+              >
+                <StepActionWrapper>
+                  <CopyPlus className="h-4 w-4" />
+                  {t('Duplicate')}
+                </StepActionWrapper>
+              </DropdownMenuItem>
+            )}
+
+            {isAction && (
+              <>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem
+                  onSelect={(e) => {
+                    e.preventDefault();
+                    deleteStep();
+                    setOpenStepActionsMenu(false);
+                    setAllowCanvasPanning(true);
+                  }}
+                >
+                  <StepActionWrapper>
+                    <Trash className="mr-2 h-4 w-4 text-destructive" />
+                    <span className="text-destructive">{t('Delete')}</span>
+                  </StepActionWrapper>
+                </DropdownMenuItem>
+              </>
+            )}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    );
+  },
+);
+
+CanvasContextMenu.displayName = 'CanvasContextMenu';
+export { CanvasContextMenu };
