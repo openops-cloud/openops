06d23850a12aecb7ba02f27553b46488aba85ee8
Cezar Dascal <cezar@openops.com>
2025-04-04 19:20:08 +0300

    Small refactor

--- packages/blocks/snowflake/src/lib/actions/run-query.ts
+++ packages/blocks/snowflake/src/lib/actions/run-query.ts
@@ -1,42 +1,45 @@
 import { createAction, Property } from '@openops/blocks-framework';
 import snowflake from 'snowflake-sdk';
+import {
+  DEFAULT_APPLICATION_NAME,
+  DEFAULT_QUERY_TIMEOUT,
+} from '../common/constants';
 import { customAuth } from '../common/custom-auth';
 
-const DEFAULT_APPLICATION_NAME = 'ActivePieces';
-const DEFAULT_QUERY_TIMEOUT = 30000;
+const props = {
+  sqlText: Property.ShortText({
+    displayName: 'SQL query',
+    description: 'Use :1, :2… or ? placeholders to use binding parameters.',
+    required: true,
+  }),
+  binds: Property.Array({
+    displayName: 'Parameters',
+    description:
+      'Binding parameters for the SQL query (to prevent SQL injection attacks)',
+    required: false,
+  }),
+  timeout: Property.Number({
+    displayName: 'Query timeout (ms)',
+    description:
+      'An integer indicating the maximum number of milliseconds to wait for a query to complete before timing out.',
+    required: false,
+    defaultValue: DEFAULT_QUERY_TIMEOUT,
+  }),
+  application: Property.ShortText({
+    displayName: 'Application name',
+    description:
+      'A string indicating the name of the client application connecting to the server.',
+    required: false,
+    defaultValue: DEFAULT_APPLICATION_NAME,
+  }),
+};
 
 export const runQuery = createAction({
   name: 'runQuery',
   displayName: 'Run Query',
   description: 'Run Query',
   auth: customAuth,
-  props: {
-    sqlText: Property.ShortText({
-      displayName: 'SQL query',
-      description: 'Use :1, :2… or ? placeholders to use binding parameters.',
-      required: true,
-    }),
-    binds: Property.Array({
-      displayName: 'Parameters',
-      description:
-        'Binding parameters for the SQL query (to prevent SQL injection attacks)',
-      required: false,
-    }),
-    timeout: Property.Number({
-      displayName: 'Query timeout (ms)',
-      description:
-        'An integer indicating the maximum number of milliseconds to wait for a query to complete before timing out.',
-      required: false,
-      defaultValue: DEFAULT_QUERY_TIMEOUT,
-    }),
-    application: Property.ShortText({
-      displayName: 'Application name',
-      description:
-        'A string indicating the name of the client application connecting to the server.',
-      required: false,
-      defaultValue: DEFAULT_APPLICATION_NAME,
-    }),
-  },
+  props,
   async run(context) {
     const { username, password, role, database, warehouse, account } =
       context.auth;
