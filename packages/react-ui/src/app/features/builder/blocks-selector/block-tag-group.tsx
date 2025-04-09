476a19eb7ce534d29a44a5e5a0d90ad22ce0c1cb
Roman Snapko <roman@openops.com>
2025-04-09 13:59:02 +0200

    feat: add new block category filters

--- packages/react-ui/src/app/features/builder/blocks-selector/block-tag-group.tsx
+++ packages/react-ui/src/app/features/builder/blocks-selector/block-tag-group.tsx
@@ -1,71 +1,88 @@
 import { BlockTag } from '@/app/features/builder/blocks-selector/block-tag';
-import construction from '@/assets/img/custom/construction.png';
-import link from '@/assets/img/custom/link.png';
+import { ScrollArea, ScrollBar } from '@openops/components/ui';
+import { BlockCategory } from '@openops/shared';
+import { t } from 'i18next';
 
-export enum BlockTagEnum {
-  CORE = 'CORE',
-  APPS = 'APPS',
-  ALL = 'ALL',
-}
+export const ALL_KEY = 'All';
+
+export type TagKey = BlockCategory | string;
 
 const tags: Record<
-  BlockTagEnum,
+  TagKey,
   {
     title: string;
     color: 'green' | 'blue' | 'purple' | 'yellow' | 'pink';
-    icon?: string;
   }
 > = {
-  [BlockTagEnum.ALL]: {
+  [ALL_KEY]: {
     title: 'All',
     color: 'blue',
   },
-  [BlockTagEnum.CORE]: {
-    icon: construction,
-    title: 'Core',
+  [BlockCategory.CLOUD]: {
+    title: 'Cloud',
+    color: 'green',
+  },
+  [BlockCategory.COMMUNICATION]: {
+    title: 'Communication',
+    color: 'purple',
+  },
+  [BlockCategory.CORE]: {
+    title: 'Utils',
+    color: 'yellow',
+  },
+  [BlockCategory.DATA]: {
+    title: 'Data',
     color: 'pink',
   },
-
-  [BlockTagEnum.APPS]: {
-    icon: link,
-    title: 'Apps',
+  [BlockCategory.FINOPS]: {
+    title: 'FinOps',
+    color: 'green',
+  },
+  [BlockCategory.IaC]: {
+    title: 'IaC',
+    color: 'blue',
+  },
+  [BlockCategory.NETWORK]: {
+    title: 'Network',
+    color: 'purple',
+  },
+  [BlockCategory.PROJECT_MANAGEMENT]: {
+    title: 'Project Management',
     color: 'yellow',
+  },
+  [BlockCategory.WORKFLOW]: {
+    title: 'Workflow',
+    color: 'green',
   },
 };
 type BlockTagGroupProps = {
   type: 'action' | 'trigger';
-  selectedTag?: BlockTagEnum;
-  onSelectTag: (tag: BlockTagEnum) => void;
+  selectedTag?: TagKey;
+  onSelectTag: (tag: TagKey) => void;
 };
 
-const BlockTagGroup = ({
-  selectedTag,
-  onSelectTag,
-  type,
-}: BlockTagGroupProps) => {
+const BlockTagGroup = ({ selectedTag, onSelectTag }: BlockTagGroupProps) => {
   return (
-    <div className="flex py-2 px-2 gap-2 items-center">
-      {Object.entries(tags).map(([tag, tagData]) => (
-        <BlockTag
-          key={tagData.title}
-          variant={tagData.color}
-          onClick={(e) => {
-            onSelectTag(
-              selectedTag === tag ? BlockTagEnum.ALL : (tag as BlockTagEnum),
-            );
-            e.stopPropagation();
-          }}
-          selected={selectedTag === tag}
-        >
-          <div className="flex items-center gap-2">
-            {tagData.icon && (
-              <img src={tagData.icon} alt={tagData.title} className="w-4 h-4" />
-            )}
-            {tagData.title}
-          </div>
-        </BlockTag>
-      ))}
-    </div>
+    <ScrollArea>
+      <div className="flex py-2 px-2 items-center mb-1">
+        {Object.entries(tags).map(([tag, tagData]) => (
+          <BlockTag
+            key={tagData.title}
+            variant={tagData.color}
+            onClick={(e) => {
+              onSelectTag(selectedTag === tag ? ALL_KEY : tag);
+              e.stopPropagation();
+            }}
+            selected={selectedTag === tag}
+          >
+            <div className="flex items-center w-fit text-nowrap">
+              {t(tagData.title)}
+            </div>
+          </BlockTag>
+        ))}
+      </div>
+      <ScrollBar orientation="horizontal" />
+    </ScrollArea>
   );
 };
 
