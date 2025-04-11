9c1d5529b7dde329d7405cba27e5d313bd2a9475
Marcelo Gonçalves <marcelo@openops.com>
2025-04-11 17:50:27 +0100

    WIP

--- packages/blocks/math-helper/jest.config.ts
+++ packages/blocks/math-helper/jest.config.ts
@@ -5,7 +5,6 @@ export default {
   ...baseConfig,
   displayName: 'blocks-math-helper',
   preset: '../../../jest.preset.js',
-  setupFiles: ['../../../jest.env.js'],
   testEnvironment: 'node',
   transform: {
     '^.+\\.ts$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
