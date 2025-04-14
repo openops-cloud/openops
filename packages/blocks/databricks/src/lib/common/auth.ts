a411f9800511fc23c46e84b01b7d7017623de4c6
Roman Snapko <roman@openops.com>
2025-04-14 09:13:06 +0200

    feat: add description to databricks connection

--- packages/blocks/databricks/src/lib/common/auth.ts
+++ packages/blocks/databricks/src/lib/common/auth.ts
@@ -1,8 +1,23 @@
 import { BlockAuth, Property } from '@openops/blocks-framework';
 import { getDatabricsToken } from './get-databrics-token';
 
+const description = `
+1. Go to the Databricks Account Console:
+\`\`\`url
+https://accounts.cloud.databricks.com/
+\`\`\`
+2. In the top-right corner, click the dropdown next to your username and copy your **Account ID**.
+3. Navigate to **User Management > Service Principals**.
+4. Create or select an existing service principal.
+5. From the service principal details, copy the **Client ID**.
+6. Create a new **Client Secret** if needed and copy it securely.
+
+> These credentials will be used to generate a secure OAuth token to access Databricks REST APIs.
+`;
+
 export const databricksAuth = BlockAuth.CustomAuth({
   required: true,
+  description,
   props: {
     accountId: Property.ShortText({
       displayName: 'Account ID',
