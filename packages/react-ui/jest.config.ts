4e302a1acec85f36d74532ca0f287d22cfbea31c
Marcelo Gonçalves <marcelo@openops.com>
2025-04-15 12:44:12 +0100

    Fix lint

--- packages/react-ui/jest.config.ts
+++ packages/react-ui/jest.config.ts
@@ -6,9 +6,7 @@ export default {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
-  transformIgnorePatterns: [
-    'node_modules/(?!(lodash-es)/)',
-  ],
+  transformIgnorePatterns: ['node_modules/(?!(lodash-es)/)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/packages/react-ui',
   moduleNameMapper: {
