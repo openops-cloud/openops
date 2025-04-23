462cc5485053a5e9fa95b5219940948e90cf3f51
Roman Snapko <roman@openops.com>
2025-04-23 12:59:51 +0200

    feat: add sql execution

--- packages/blocks/google-cloud/src/lib/actions/execute-sql-query.ts
+++ packages/blocks/google-cloud/src/lib/actions/execute-sql-query.ts
@@ -1,10 +1,13 @@
-import { createAction } from '@openops/blocks-framework';
+import { createAction, Property, Validators } from '@openops/blocks-framework';
 import {
   dryRunCheckBox,
   getUseHostSessionProperty,
   googleCloudAuth,
+  handleCliError,
+  tryParseJson,
 } from '@openops/common';
 import { projectCliDropdown } from '../common-properties';
+import { runCommand } from '../google-cloud-cli';
 
 export const executeSqlQuery = createAction({
   auth: googleCloudAuth,
@@ -17,9 +20,50 @@ export const executeSqlQuery = createAction({
       'gcloud auth login',
     ),
     project: projectCliDropdown,
+    sqlText: Property.ShortText({
+      displayName: 'SQL query',
+      required: true,
+      description:
+        'The SQL statement to execute. You can use named parameters like `:name` or numbered placeholders like `:1`, `:2`, etc.',
+    }),
+    params: Property.Object({
+      displayName: 'Parameters',
+      required: false,
+      description:
+        'Optional parameter values to bind to the SQL query. Use a key-value structure where keys match named parameters or position indices.',
+    }),
+    timeout: Property.Number({
+      displayName: 'Query timeout',
+      description:
+        'Maximum number of seconds to wait for a query to complete before timing out. Min value is 5',
+      required: true,
+      validators: [Validators.number, Validators.minValue(0)],
+      defaultValue: 60,
+    }),
     dryRun: dryRunCheckBox(),
   },
-  async run(context) {
-    return context.auth;
+  async run({ propsValue, auth }) {
+    const { project, sqlText, timeout, params, dryRun } = propsValue;
+
+    if (dryRun) {
+      return `Step execution skipped, dry run flag enabled. Google Cloud CLI command will not be executed. Command: '${sqlText}'`;
+    }
+
+    try {
+      const result = await runCommand(
+        `bq query --nouse_legacy_sql '${sqlText}'`,
+        auth,
+        propsValue.useHostSession?.['useHostSessionCheckbox'],
+        project,
+        'bq',
+      );
+      return tryParseJson(result);
+    } catch (error) {
+      handleCliError({
+        provider: 'Google Cloud',
+        command: sqlText,
+        error,
+      });
+    }
   },
 });
