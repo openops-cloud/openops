7f943b40cc7ad3a7c57e21b2f2ee3d600207260b
Cezar Dascal <cezar@openops.com>
2025-05-07 11:54:53 +0200

    Control usage of the new test output API via flag

--- packages/react-ui/src/app/features/builder/test-step/use-step-test-output.ts
+++ packages/react-ui/src/app/features/builder/test-step/use-step-test-output.ts
@@ -1,4 +1,5 @@
-import { Action, Trigger } from '@openops/shared';
+import { flagsHooks } from '@/app/common/hooks/flags-hooks';
+import { Action, FlagId, Trigger } from '@openops/shared';
 import { useQuery } from '@tanstack/react-query';
 import { UseFormReturn, useWatch } from 'react-hook-form';
 import { flowsApi } from '../../flows/lib/flows-api';
@@ -7,21 +8,25 @@ export const useStepTestOuput = (
   flowVersionId: string,
   form: UseFormReturn<Action> | UseFormReturn<Trigger>,
 ) => {
+  const { data: useNewExternalTestData = false } = flagsHooks.useFlag(
+    FlagId.USE_NEW_EXTERNAL_TESTDATA,
+  );
   const fallbackData = useWatch({
     name: 'settings.inputUiInfo.currentSelectedData',
     control: form.control as any,
   }) as unknown;
-  const { id } = form.getValues();
+  const { id: stepId } = form.getValues();
 
   return useQuery({
-    queryKey: ['actionTestOutput', flowVersionId, id],
+    queryKey: ['actionTestOutput', flowVersionId, stepId],
     queryFn: async () => {
-      if (!id) {
+      if (!stepId || !useNewExternalTestData) {
         return fallbackData;
       }
 
       return (
-        (await flowsApi.getStepTestOutput(flowVersionId, id)) ?? fallbackData
+        (await flowsApi.getStepTestOutput(flowVersionId, stepId)) ??
+        fallbackData
       );
     },
   });
