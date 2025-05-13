9be2af01d7e1a835c1938c8c570751cc93812c3e
Maor Rozenfeld <49363375+maor-rozenfeld@users.noreply.github.com>
2025-05-13 17:25:52 +0200

    Add debug logs for code piece

--- packages/engine/src/lib/core/code/v8-isolate-code-sandbox.ts
+++ packages/engine/src/lib/core/code/v8-isolate-code-sandbox.ts
@@ -22,12 +22,15 @@ const getIvm = () => {
  */
 export const v8IsolateCodeSandbox: CodeSandbox = {
   async runCodeModule({ codeFile, inputs }) {
+    logger.debug('Getting IVM...')
     const ivm = getIvm();
+    logger.debug('Creating isolate object...')
     const isolate = new ivm.Isolate({
       memoryLimit: BLOCK_MEMORY_LIMIT_IN_MB,
     });
 
     try {
+      logger.debug('Creating isolate context...')
       const isolateContext = await initIsolateContext({
         isolate,
         codeContext: {
@@ -35,6 +38,7 @@ export const v8IsolateCodeSandbox: CodeSandbox = {
         },
       });
 
+      logger.debug('Reading code file...')
       const code = await fs.readFile(codeFile, 'utf8');
 
       logger.debug('Running code module in V8 Isolate sandbox', { code });
