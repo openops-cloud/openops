8a67f4ff4e2c57b86a417f13af71eb1b78282b7d
Roman Snapko <roman@openops.com>
2025-05-14 09:51:10 +0200

    feat: update icon

--- packages/ui-components/src/components/ai-chat-container/no-ai-enabled-popover.tsx
+++ packages/ui-components/src/components/ai-chat-container/no-ai-enabled-popover.tsx
@@ -7,7 +7,9 @@ const NoAiEnabledPopover = ({ className }: { className?: string }) => {
   return (
     <div className={cn('bg-background shadow-editor rounded-lg', className)}>
       <div className="h-[58px] px-4 py-[10px] rounded-t-xl flex items-center gap-3 bg-white dark:bg-black dark:text-white border-b">
-        <Bot className="w-6 h-6 dark:text-primary" />
+        <div className="size-8 flex justify-center items-center bg-background bg-gradient-to-b from-ring/40 to-primary-200/40 rounded-xl">
+          <Bot size={20} />
+        </div>
         <h2 className="font-bold text-base">{t('AI Assistant')}</h2>
       </div>
       <div className="w-[323px] pl-[26px] py-4 text-sm bg-secondary/10 dark:text-white">
