089e35176568741fc78b2f31f0791e40527b2ff3
Roman Snapko <roman@openops.com>
2025-05-14 09:39:48 +0200

    fix: dark theme

--- packages/ui-components/src/components/ai-chat-container/no-ai-enabled-popover.tsx
+++ packages/ui-components/src/components/ai-chat-container/no-ai-enabled-popover.tsx
@@ -6,11 +6,11 @@ import { cn } from '../../lib/cn';
 const NoAiEnabledPopover = ({ className }: { className?: string }) => {
   return (
     <div className={cn('bg-background shadow-editor rounded-lg', className)}>
-      <div className="h-[58px] px-4 py-[10px] rounded-t-xl flex items-center gap-3 bg-white dark:bg-black border-b">
+      <div className="h-[58px] px-4 py-[10px] rounded-t-xl flex items-center gap-3 bg-white dark:bg-black dark:text-white border-b">
         <Bot className="w-6 h-6 dark:text-primary" />
         <h2 className="font-bold text-base">{t('AI Assistant')}</h2>
       </div>
-      <div className="w-[323px] pl-[26px] py-4 text-sm bg-secondary/10">
+      <div className="w-[323px] pl-[26px] py-4 text-sm bg-secondary/10 dark:text-white">
         <span>
           {t('It looks like AI hasn’t been configured in OpenOps yet.')}
         </span>
