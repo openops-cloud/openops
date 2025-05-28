e05e365bd6961d2e6bc6bf88340a054beead0128
Cezar Dascal <cezar@openops.com>
2025-05-28 19:49:26 +0300

    Remove unused code

--- packages/shared/src/lib/flows/folders/folder-requests.ts
+++ packages/shared/src/lib/flows/folders/folder-requests.ts
@@ -1,5 +1,4 @@
 import { Static, Type } from '@sinclair/typebox';
-import { Cursor } from '../../common/seek-page';
 
 export const CreateFolderRequest = Type.Object({
   displayName: Type.String(),
@@ -21,17 +20,6 @@ export const DeleteFolderRequest = Type.Object({
 });
 
 export type DeleteFlowRequest = Static<typeof DeleteFolderRequest>;
-
-export const ListFolderRequest = Type.Object({
-  limit: Type.Optional(Type.Number({})),
-  cursor: Type.Optional(Type.String({})),
-  projectId: Type.String(),
-});
-
-export type ListFolderRequest = Omit<
-  Static<typeof ListFolderRequest>,
-  'cursor'
-> & { cursor: Cursor | undefined };
 
 export const ListFolderFlowsRequest = Type.Object({
   excludeUncategorizedFolder: Type.Optional(Type.Boolean()),
