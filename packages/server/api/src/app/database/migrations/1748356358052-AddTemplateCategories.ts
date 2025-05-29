29c83dc9334041857d3999f2c700a5ce22dff220
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2025-05-29 14:40:21 +0300

    add default

--- packages/server/api/src/app/database/migrations/1748356358052-AddTemplateCategories.ts
+++ packages/server/api/src/app/database/migrations/1748356358052-AddTemplateCategories.ts
@@ -6,11 +6,11 @@ export class AddTemplateCategories1748356358052 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
             ALTER TABLE "flow_template"
-            ADD "categories" jsonb
+            ADD "categories" jsonb DEFAULT '[]'
         `);
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {
+  public async down(_: QueryRunner): Promise<void> {
     throw new Error('Rollback not implemented');
   }
 }
