0e4323aa466f37b36e06193be15e20f0ce83c365
Cezar Dascal <cezar@openops.com>
2025-05-30 18:43:32 +0300

    Show disclaimer note to user in post login popup about insecure connections failing cloud templates login

---
+++ packages/ui-components/src/components/cloud-connection/cloud-logged-in-brief.tsx
@@ -0,0 +1,26 @@
+import { t } from 'i18next';
+import { User } from 'lucide-react';
+import { ReactNode } from 'react';
+import { Alert, AlertDescription } from '../../ui/alert';
+
+export const CloudLoggedInBrief = ({ appLogo }: { appLogo: ReactNode }) => (
+  <div className="flex flex-col items-center h-[100vh] p-5 gap-4">
+    {appLogo}
+    <div className="w-[42px] h-[42px] mt-[100px] bg-gray-300 rounded-full flex items-center justify-center">
+      <User />
+    </div>
+    <h1 className="text-base text-center">
+      {t('You are now logged in,')}
+      <br />
+      {t('you can close this window anytime.')}
+    </h1>
+    <Alert className="mt-8 text-sm flex-col" variant="default">
+      <AlertDescription>
+        <b>{t('Note: ')}</b>
+        {t(
+          "Exploring template catalog might not work if you're not running Openops over a secure HTTPS connection.",
+        )}
+      </AlertDescription>
+    </Alert>
+  </div>
+);
