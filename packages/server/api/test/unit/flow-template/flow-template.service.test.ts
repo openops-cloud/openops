45451b2a2c9b042a536a521d68650e9c1fccc55f
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2025-06-02 20:00:26 +0300

    fix test

--- packages/server/api/test/unit/flow-template/flow-template.service.test.ts
+++ packages/server/api/test/unit/flow-template/flow-template.service.test.ts
@@ -202,6 +202,7 @@ describe('flowTemplateService', () => {
         services: ['service1'],
         domains: ['domain1'],
         blocks: ['block1'],
+        categories: ['category1', 'category2'],
         organizationId: 'organization-id',
         projectId: 'project-id',
       };
@@ -253,6 +254,7 @@ describe('flowTemplateService', () => {
         services: ['service1'],
         domains: ['domain1'],
         blocks: ['block1'],
+        categories: ['category1', 'category2'],
         organizationId: 'organization-id',
         projectId: 'project-id',
       };
