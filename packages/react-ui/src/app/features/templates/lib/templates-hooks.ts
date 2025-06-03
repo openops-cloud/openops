48d0d1b06b19236c921b3d91f6b7b03bd154555f
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2025-06-03 17:22:05 +0300

    improve regex

--- packages/react-ui/src/app/features/templates/lib/templates-hooks.ts
+++ packages/react-ui/src/app/features/templates/lib/templates-hooks.ts
@@ -47,7 +47,7 @@ const sortFunction = (a: string, b: string) => {
 // replace services like "AWS EC2" or "GCP .." or "Azure .." with just "EC2"
 export const replaceServicePrefix = (service?: string) => {
   if (!service) return '';
-  const match = service.match(/^(AWS|GCP|Azure)\s+(.*)$/);
+  const match = service.match(/^(AWS|GCP|Azure)\s+([^\s].*)$/);
   return match ? match[2] : service;
 };
 
