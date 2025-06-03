54d72604c6af67bdc7c327ced24f3964aae860f1
Marcelo Gonçalves <marcelo@openops.com>
2025-06-03 20:28:13 +0100

    WIP

--- packages/blocks/framework/src/lib/property/authentication/common.ts
+++ packages/blocks/framework/src/lib/property/authentication/common.ts
@@ -1,12 +1,14 @@
-import { ConnectionProvider, Provider } from '@openops/shared';
+import { Provider } from '@openops/shared';
 import { Type } from '@sinclair/typebox';
 
 export const BaseBlockAuthSchema = Type.Object({
+  provider: Provider,
   displayName: Type.String(),
   description: Type.Optional(Type.String()),
 });
 
 export type BaseBlockAuthSchema<AuthValueSchema> = {
+  provider: Provider;
   displayName: string;
   description?: string;
   validate?: (params: {
