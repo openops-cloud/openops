c566fe96e6038f5d7b4c0b1603b9e2aee22074a1
Maor Rozenfeld <49363375+maor-rozenfeld@users.noreply.github.com>
2025-06-04 14:09:55 +0200

    Prop

--- packages/blocks/aws/src/lib/actions/cli/aws-cli.ts
+++ packages/blocks/aws/src/lib/actions/cli/aws-cli.ts
@@ -1,12 +1,15 @@
 import { runCliCommand } from '@openops/common';
-import { system } from '@openops/server-shared';
+import { SharedSystemProp, system } from '@openops/server-shared';
 
 export async function runCommand(
   command: string,
   region: string,
   credentials: any,
 ): Promise<string> {
-  if (credentials.accessKeyId === undefined && !system.getBoolean('AWS_ENABLE_IMPLICIT_ROLE')) {
+  if (
+    credentials.accessKeyId === undefined &&
+    !system.getBoolean(SharedSystemProp.AWS_ENABLE_IMPLICIT_ROLE)
+  ) {
     throw new Error(
       'AWS credentials are required, please provide accessKeyId and secretAccessKey',
     );
