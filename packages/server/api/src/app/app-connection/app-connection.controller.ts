20eca3a83803a15d9f82a0a42a6dd6e14a99890e
Marcelo Gonçalves <marcelo@openops.com>
2025-06-04 14:25:11 +0100

    Prepare for blockName removal

--- packages/server/api/src/app/app-connection/app-connection.controller.ts
+++ packages/server/api/src/app/app-connection/app-connection.controller.ts
@@ -1,7 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { FastifyPluginCallbackTypebox, Type } from '@fastify/type-provider-typebox';
+import {
+  FastifyPluginCallbackTypebox,
+  Type,
+} from '@fastify/type-provider-typebox';
 import {
   AppConnectionWithoutSensitiveData,
+  FlagId,
   ListAppConnectionsRequestQuery,
   OpenOpsId,
   PatchAppConnectionRequestBody,
@@ -13,9 +17,11 @@ import {
 } from '@openops/shared';
 import { StatusCodes } from 'http-status-codes';
 import { blockMetadataService } from '../blocks/block-metadata-service';
+import { devFlagsService } from '../flags/dev-flags.service';
 import { sendConnectionDeletedEvent } from '../telemetry/event-models';
 import { appConnectionService } from './app-connection-service/app-connection-service';
 import { redactSecrets, removeSensitiveData } from './app-connection-utils';
+import { resolveProvidersForBlocks } from './connection-providers-resolver';
 
 export const appConnectionController: FastifyPluginCallbackTypebox = (
   app,
@@ -64,9 +70,25 @@ export const appConnectionController: FastifyPluginCallbackTypebox = (
     '/',
     ListAppConnectionsRequest,
     async (request): Promise<SeekPage<AppConnectionWithoutSensitiveData>> => {
-      const { name, blockNames, status, cursor, limit, providers } = request.query;
+      const { name, status, cursor, limit } = request.query;
+      let { blockNames, providers } = request.query;
+
+      const featureFlag = await devFlagsService.getOne(
+        FlagId.USE_CONNECTIONS_PROVIDER,
+      );
+      if (blockNames && featureFlag?.value) {
+        const blockProviders = await resolveProvidersForBlocks(
+          blockNames,
+          request.principal.projectId,
+        );
+
+        blockNames = [];
+        providers = providers ?? [];
+        providers.push(...blockProviders);
+      }
 
       const appConnections = await appConnectionService.list({
+        // TODO: remove blockNames from this request
         blockNames,
         name,
         status,
