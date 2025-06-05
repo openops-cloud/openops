559bd87fc0d5044661cf38a8d86152ebd1818a35
Maor Rozenfeld <maor@openops.com>
2025-06-05 12:18:46 +0200

    Fix build

--- packages/openops/src/lib/aws/auth.ts
+++ packages/openops/src/lib/aws/auth.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
 import { BlockAuth, Property } from '@openops/blocks-framework';
 import { logger, SharedSystemProp, system } from '@openops/server-shared';
-import { getConnectionProvider, Provider } from '@openops/shared';
 import { parseArn } from './arn-handler';
 import { assumeRole } from './sts-common';
 
