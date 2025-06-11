7543115e67e894337606926a412b11e210d3ce8a
Leyla <54935347+bigfluffycookie@users.noreply.github.com>
2025-06-11 17:18:49 +0200

    Update flow-version.controller.ts

--- packages/server/api/src/app/flows/flow/flow-version.controller.ts
+++ packages/server/api/src/app/flows/flow/flow-version.controller.ts
@@ -198,7 +198,7 @@ const GetLatestVersionsByConnectionRequestOptions = {
   },
   schema: {
     description:
-      'Retrieves all workflow that contain a specific connection. This endpoint returns the latest version of each workflow that uses the specified connection, including minimal workflow information such as ID, name, and version details. Useful for tracking which workflow depend on a particular connection.',
+      'Retrieves all workflows that contain a specific connection. This endpoint returns the latest version of each workflow that uses the specified connection, including minimal workflow information such as ID, name, and version details. Useful for tracking which workflows depend on a particular connection.',
     tags: ['flow-version'],
     security: [SERVICE_KEY_SECURITY_OPENAPI],
     querystring: Type.Object({
