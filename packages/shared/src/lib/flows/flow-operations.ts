752212f4744bf546c362b5d761733b06d35ec406
Marcelo Gonçalves <marcelo@openops.com>
2025-06-25 17:03:55 +0100

    Fix lint

--- packages/shared/src/lib/flows/flow-operations.ts
+++ packages/shared/src/lib/flows/flow-operations.ts
@@ -9,7 +9,7 @@ import {
   SplitActionSchema,
 } from './actions/action';
 import { FlowStatus } from './flow';
-import { Trigger, TriggerWithOptionalId, UpdateTriggerRequest } from './triggers/trigger';
+import { Trigger, UpdateTriggerRequest } from './triggers/trigger';
 
 export enum FlowOperationType {
   LOCK_AND_PUBLISH = 'LOCK_AND_PUBLISH',
