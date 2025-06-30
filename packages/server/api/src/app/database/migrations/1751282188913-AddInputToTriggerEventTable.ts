7651d399e1fbcf1a39a8fbfa961fb628f8b89a9d
Roman Snapko <roman@openops.com>
2025-06-30 13:17:39 +0200

    chore: rename

--- packages/server/api/src/app/database/migrations/1751282188913-AddInputToTriggerEventTable.ts
+++ packages/server/api/src/app/database/migrations/1751282188913-AddInputToTriggerEventTable.ts
@@ -1,18 +1,18 @@
 import { logger } from '@openops/server-shared';
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AddInputToTriggerEventTable1750933522145
+export class AddInputToTriggerEventTable1751282188913
   implements MigrationInterface
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    logger.info('AddInputToTriggerEventTable1750933522145: starting');
+    logger.info('AddInputToTriggerEventTable1751282188913: starting');
 
     await queryRunner.query(`
       ALTER TABLE "trigger_event"
       ADD COLUMN IF NOT EXISTS "input" jsonb;
     `);
 
-    logger.info('AddInputToTriggerEventTable1750933522145: completed');
+    logger.info('AddInputToTriggerEventTable1751282188913: completed');
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
