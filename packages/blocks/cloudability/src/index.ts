9779e0aee8dacd92f5baf769fcae173e1dcf3e95
Rita Gorokhod <60586879+rita-gorokhod@users.noreply.github.com>
2025-07-01 17:31:40 +0000

    Move apiUrl to the auth

--- packages/blocks/cloudability/src/index.ts
+++ packages/blocks/cloudability/src/index.ts
@@ -3,26 +3,7 @@ import { createBlock, Property } from '@openops/blocks-framework';
 import { BlockCategory } from '@openops/shared';
 import { cloudabilityAuth } from './lib/auth';
 
-const markdown = `When making calls to the regions, the API subdomain changes:
-
-- **US:**  
-  \`\`\`
-  https://api.cloudability.com/v3/business-mappings
-  \`\`\`
-- **EMEA:**  
-  \`\`\`
-  https://api-eu.cloudability.com/v3/business-mappings
-  \`\`\`
-- **Middle East:**  
-  \`\`\`
-  https://api-me.cloudability.com/v3/business-mappings
-  \`\`\`
-- **APAC:**  
-  \`\`\`
-  https://api-au.cloudability.com/v3/business-mappings
-  \`\`\`
-
-
+const markdown = `
 For more information, visit the [Cloudability API documentation](https://www.ibm.com/docs/en/cloudability-commercial/cloudability-standard/saas?topic=api-getting-started-cloudability-v3).
 `;
 
@@ -35,10 +16,10 @@ export const cloudability = createBlock({
   categories: [BlockCategory.FINOPS],
   actions: [
     createCustomApiCallAction({
-      baseUrl: () => 'https://api.cloudability.com',
+      baseUrl: (auth: any) => auth.apiUrl,
       auth: cloudabilityAuth,
-      authMapping: async (context) => {
-        const apiKey = context.auth as string;
+      authMapping: async (context: any) => {
+        const { apiKey } = context.auth;
         const encoded = Buffer.from(`${apiKey}:`).toString('base64');
         return {
           Authorization: `Basic ${encoded}`,
