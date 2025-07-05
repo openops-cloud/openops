19aa1560a4db464aa02bea15674d0e19f24d18bd
Cezar Dascal <cezar@openops.com>
2025-07-05 17:00:49 +0300

    Subtitute only NGINX_CLIENT_MAX_BODY_SIZE to prevent map directive from getting broken

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 export NGINX_CLIENT_MAX_BODY_SIZE="${OPS_REQUEST_BODY_LIMIT:-10}m"
 
 # Generate nginx config from template
-envsubst < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
+envsubst '${NGINX_CLIENT_MAX_BODY_SIZE}' < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
 
 # Start Nginx server
 nginx -g "daemon off;" &
