72846a56da891d6f9f60c86f49b1ddbcea7a3ffc
Rita Gorokhod <60586879+rita-gorokhod@users.noreply.github.com>
2025-07-15 21:11:08 +0000

    Remove additional filters

--- packages/blocks/cloudhealth/src/lib/actions/get-recommendations-action.ts
+++ packages/blocks/cloudhealth/src/lib/actions/get-recommendations-action.ts
@@ -1,13 +1,6 @@
-import {
-  createAction,
-  Property,
-  StaticDropdownProperty,
-} from '@openops/blocks-framework';
+import { createAction, Property } from '@openops/blocks-framework';
 import { cloudhealthAuth } from '../auth';
 import { executeGraphQLQuery } from '../common/execute-graphql-query';
-import { extractErrorMessage } from '../common/extract-error-message';
-import { getAwsAccounts } from '../common/get-aws-accounts';
-import { getAzureSubscriptions } from '../common/get-azure-subscriptions';
 import {
   ASSET_CONFIGS,
   generateQuery,
@@ -30,44 +23,6 @@ export const getRecommendationsAction = createAction({
         })),
       },
     }),
-    additionalFilters: Property.DynamicProperties({
-      displayName: 'Additional Filters',
-      description: 'Additional filters to apply to the recommendations.',
-      required: false,
-      refreshers: ['recommendationType'],
-      props: async ({ auth, recommendationType }: any) => {
-        const mandatoryFieldNames =
-          ASSET_CONFIGS[recommendationType as keyof typeof ASSET_CONFIGS]
-            .mandatoryFields;
-
-        if (!mandatoryFieldNames || mandatoryFieldNames.length === 0) {
-          return {} as any;
-        }
-
-        const result: Record<string, any> = {};
-        for (const field of mandatoryFieldNames) {
-          switch (field) {
-            case 'aws_account_id': {
-              result['aws_account_id'] = await aws_account_id(auth);
-              break;
-            }
-            case 'azure_subscription_id': {
-              result['azure_subscription_id'] = await azure_subscription_id(
-                auth,
-              );
-              break;
-            }
-            case 'cloud_provider':
-              result['cloud_provider'] = cloud_provider();
-              break;
-            default:
-              break;
-          }
-        }
-
-        return result;
-      },
-    }),
     evaluationDuration: Property.StaticDropdown({
       displayName: 'Evaluation Duration',
       description: 'The duration for which to get the recommendations.',
@@ -85,12 +40,12 @@ export const getRecommendationsAction = createAction({
     }),
   },
   async run(context) {
-    const { recommendationType, additionalFilters, evaluationDuration } =
+    const { recommendationType, evaluationDuration } =
       context.propsValue as any;
 
     const { query, variables } = generateQuery(
       recommendationType,
-      additionalFilters,
+      {},
       evaluationDuration,
     );
 
@@ -103,76 +58,3 @@ export const getRecommendationsAction = createAction({
     });
   },
 });
-
-async function aws_account_id(
-  apiKey: string,
-): Promise<StaticDropdownProperty<string, boolean>> {
-  let awsAccounts: unknown[] = [];
-  let error: string | undefined;
-  let disabled = false;
-
-  try {
-    awsAccounts = await getAwsAccounts(apiKey);
-  } catch (e) {
-    disabled = true;
-    error = extractErrorMessage(e);
-  }
-
-  return Property.StaticDropdown({
-    displayName: 'Filter by AWS Account ID',
-    description: 'The AWS account ID to filter recommendations by.',
-    required: false,
-    options: {
-      error: error,
-      disabled: disabled,
-      options: awsAccounts.map((account: any) => ({
-        label: account.name,
-        value: account.id,
-      })),
-    },
-  });
-}
-
-async function azure_subscription_id(
-  apiKey: string,
-): Promise<StaticDropdownProperty<string, boolean>> {
-  let azureSubscriptions: unknown[] = [];
-  let error: string | undefined;
-  let disabled = false;
-
-  try {
-    azureSubscriptions = await getAzureSubscriptions(apiKey);
-  } catch (e) {
-    disabled = true;
-    error = extractErrorMessage(e);
-  }
-
-  return Property.StaticDropdown({
-    displayName: 'Filter by Azure Subscription ID',
-    description: 'The Azure subscription ID to filter recommendations by.',
-    required: false,
-    options: {
-      error: error,
-      disabled: disabled,
-      options: azureSubscriptions.map((subscription: any) => ({
-        label: subscription.name,
-        value: subscription.id,
-      })),
-    },
-  });
-}
-
-function cloud_provider(): StaticDropdownProperty<string, boolean> {
-  return Property.StaticDropdown({
-    displayName: 'Cloud Provider',
-    description: 'The cloud provider to filter recommendations by.',
-    required: true,
-    options: {
-      options: [
-        { label: 'AWS', value: 'aws' },
-        { label: 'Azure', value: 'azure' },
-        { label: 'GCP', value: 'gcp' },
-      ],
-    },
-  });
-}
