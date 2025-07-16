ef48ceb704ff055503613fe29dea43d507415a16
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2025-07-16 11:28:32 +0300

    types deprecation

--- packages/ui-components/src/components/ai-chat-messages/types.ts
+++ packages/ui-components/src/components/ai-chat-messages/types.ts
@@ -1,20 +1,28 @@
 import { SourceCode } from '@openops/shared';
 
+/**
+ * @deprecated use shared types instead from ai sdk
+ */
 export enum AIChatMessageRole {
   user = 'user',
   assistant = 'assistant',
 }
 
+/**
+ * @deprecated use shared types instead from ai sdk
+ */
 export type AIChatMessageContent =
   | string
   | {
       parts: (
         | { type: 'text'; content: string }
-        | { type: 'code'; content: string; language?: string }
         | { type: 'sourcecode'; content: SourceCode }
       )[];
     };
 
+/**
+ * @deprecated use shared types instead from ai sdk
+ */
 export type AIChatMessage = {
   id: string;
   role: keyof typeof AIChatMessageRole;
