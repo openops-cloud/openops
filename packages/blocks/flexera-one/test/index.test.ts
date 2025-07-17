470492aa40b96b4bc050f8f06540387d45a666f3
Rita Gorokhod <60586879+rita-gorokhod@users.noreply.github.com>
2025-07-17 14:34:29 +0000

    Remove unused package

--- packages/blocks/flexera-one/test/index.test.ts
+++ packages/blocks/flexera-one/test/index.test.ts
@@ -1,19 +1,27 @@
-import { flexeraOne } from '../src/index';
+import { flexera } from '../src/index';
 
 describe('block declaration tests', () => {
   test('should return block with correct authentication', () => {
-    expect(flexeraOne.auth).toMatchObject({
+    expect(flexera.auth).toMatchObject({
       type: 'CUSTOM_AUTH',
       required: true,
-      authProviderKey: 'flexera-one',
-      authProviderDisplayName: 'Flexera-one',
-      authProviderLogoUrl: 'https://static.openops.com/blocks/flexera-one.png',
+      authProviderKey: 'Flexera-One',
+      authProviderDisplayName: 'Flexera',
+      authProviderLogoUrl: 'https://static.openops.com/blocks/flexera.png',
     });
   });
 
   test('should return block with correct number of actions', () => {
-    expect(Object.keys(flexeraOne.actions()).length).toBe(1);
-    expect(flexeraOne.actions()).toMatchObject({
+    expect(Object.keys(flexera.actions()).length).toBe(3);
+    expect(flexera.actions()).toMatchObject({
+      flexera_get_active_recommendations: {
+        name: 'flexera_get_active_recommendations',
+        requireAuth: true,
+      },
+      flexera_get_incident: {
+        name: 'flexera_get_incident',
+        requireAuth: true,
+      },
       custom_api_call: {
         name: 'custom_api_call',
         requireAuth: true,
