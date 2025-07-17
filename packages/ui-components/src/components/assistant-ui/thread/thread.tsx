e031d633de6877ac654de6baf3d339e99d985b21
Cezar Dascal <cezar@openops.com>
2025-07-17 19:23:30 +0300

    use CircleStopIcon from Lucide

--- packages/ui-components/src/components/assistant-ui/thread/thread.tsx
+++ packages/ui-components/src/components/assistant-ui/thread/thread.tsx
@@ -10,6 +10,7 @@ import {
   CheckIcon,
   ChevronLeftIcon,
   ChevronRightIcon,
+  CircleStopIcon,
   CopyIcon,
   PencilIcon,
   RefreshCwIcon,
@@ -259,19 +260,5 @@ const BranchPicker: FC<BranchPickerPrimitive.Root.Props> = ({
         </TooltipIconButton>
       </BranchPickerPrimitive.Next>
     </BranchPickerPrimitive.Root>
-  );
-};
-
-const CircleStopIcon = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 16 16"
-      fill="currentColor"
-      width="16"
-      height="16"
-    >
-      <rect width="10" height="10" x="3" y="3" rx="2" />
-    </svg>
   );
 };
