016d26555121b2f13986ff7462639a85053aee54
Cezar Dascal <cezar@openops.com>
2025-07-17 15:59:22 +0300

    Fix Sonarcloud findings

--- packages/ui-components/src/components/assistant-ui/markdown-text/markdown-text.tsx
+++ packages/ui-components/src/components/assistant-ui/markdown-text/markdown-text.tsx
@@ -63,145 +63,177 @@ const useCopyToClipboard = ({
 };
 
 const defaultComponents = memoizeMarkdownComponents({
-  h1: ({ className, ...props }) => (
+  h1: ({ className, children, ...props }) => (
     <h1
       className={cn(
         'mb-8 scroll-m-20 text-4xl font-extrabold tracking-tight last:mb-0',
         className,
       )}
       {...props}
-    />
+    >
+      {children}
+    </h1>
   ),
-  h2: ({ className, ...props }) => (
+  h2: ({ className, children, ...props }) => (
     <h2
       className={cn(
         'mb-4 mt-8 scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0 last:mb-0',
         className,
       )}
       {...props}
-    />
+    >
+      {children}
+    </h2>
   ),
-  h3: ({ className, ...props }) => (
+  h3: ({ className, children, ...props }) => (
     <h3
       className={cn(
         'mb-4 mt-6 scroll-m-20 text-2xl font-semibold tracking-tight first:mt-0 last:mb-0',
         className,
       )}
       {...props}
-    />
+    >
+      {children}
+    </h3>
   ),
-  h4: ({ className, ...props }) => (
+  h4: ({ className, children, ...props }) => (
     <h4
       className={cn(
         'mb-4 mt-6 scroll-m-20 text-xl font-semibold tracking-tight first:mt-0 last:mb-0',
         className,
       )}
       {...props}
-    />
+    >
+      {children}
+    </h4>
   ),
-  h5: ({ className, ...props }) => (
+  h5: ({ className, children, ...props }) => (
     <h5
       className={cn(
         'my-4 text-lg font-semibold first:mt-0 last:mb-0',
         className,
       )}
       {...props}
-    />
+    >
+      {children}
+    </h5>
   ),
-  h6: ({ className, ...props }) => (
+  h6: ({ className, children, ...props }) => (
     <h6
       className={cn('my-4 font-semibold first:mt-0 last:mb-0', className)}
       {...props}
-    />
+    >
+      {children}
+    </h6>
   ),
-  p: ({ className, ...props }) => (
+  p: ({ className, children, ...props }) => (
     <p
       className={cn('mb-5 mt-5 leading-7 first:mt-0 last:mb-0', className)}
       {...props}
-    />
+    >
+      {children}
+    </p>
   ),
-  a: ({ className, ...props }) => (
+  a: ({ className, children, ...props }) => (
     <a
       className={cn(
         'text-primary font-medium underline underline-offset-4',
         className,
       )}
       {...props}
-    />
+    >
+      {children}
+    </a>
   ),
-  blockquote: ({ className, ...props }) => (
-    <blockquote
-      className={cn('border-l-2 pl-6 italic', className)}
-      {...props}
-    />
+  blockquote: ({ className, children, ...props }) => (
+    <blockquote className={cn('border-l-2 pl-6 italic', className)} {...props}>
+      {children}
+    </blockquote>
   ),
-  ul: ({ className, ...props }) => (
-    <ul
-      className={cn('my-5 ml-6 list-disc [&>li]:mt-2', className)}
-      {...props}
-    />
+  ul: ({ className, children, ...props }) => (
+    <ul className={cn('my-5 ml-6 list-disc [&>li]:mt-2', className)} {...props}>
+      {children}
+    </ul>
   ),
-  ol: ({ className, ...props }) => (
+  ol: ({ className, children, ...props }) => (
     <ol
       className={cn('my-5 ml-6 list-decimal [&>li]:mt-2', className)}
       {...props}
-    />
+    >
+      {children}
+    </ol>
   ),
   hr: ({ className, ...props }) => (
-    <hr className={cn('my-5 border-b', className)} {...props} />
+    <hr
+      className={cn('my-5 border-b', className)}
+      aria-hidden="true"
+      {...props}
+    />
   ),
-  table: ({ className, ...props }) => (
+  table: ({ className, children, ...props }) => (
     <table
       className={cn(
         'my-5 w-full border-separate border-spacing-0 overflow-y-auto',
         className,
       )}
       {...props}
-    />
+    >
+      {children}
+    </table>
   ),
-  th: ({ className, ...props }) => (
+  th: ({ className, children, ...props }) => (
     <th
       className={cn(
         'bg-muted px-4 py-2 text-left font-bold first:rounded-tl-lg last:rounded-tr-lg [&[align=center]]:text-center [&[align=right]]:text-right',
         className,
       )}
       {...props}
-    />
+    >
+      {children}
+    </th>
   ),
-  td: ({ className, ...props }) => (
+  td: ({ className, children, ...props }) => (
     <td
       className={cn(
         'border-b border-l px-4 py-2 text-left last:border-r [&[align=center]]:text-center [&[align=right]]:text-right',
         className,
       )}
       {...props}
-    />
+    >
+      {children}
+    </td>
   ),
-  tr: ({ className, ...props }) => (
+  tr: ({ className, children, ...props }) => (
     <tr
       className={cn(
         'm-0 border-b p-0 first:border-t [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg',
         className,
       )}
       {...props}
-    />
+    >
+      {children}
+    </tr>
   ),
-  sup: ({ className, ...props }) => (
+  sup: ({ className, children, ...props }) => (
     <sup
       className={cn('[&>a]:text-xs [&>a]:no-underline', className)}
       {...props}
-    />
+    >
+      {children}
+    </sup>
   ),
-  pre: ({ className, ...props }) => (
+  pre: ({ className, children, ...props }) => (
     <pre
       className={cn(
         'overflow-x-auto rounded-b-lg bg-black p-4 text-white',
         className,
       )}
       {...props}
-    />
+    >
+      {children}
+    </pre>
   ),
-  code: function Code({ className, ...props }) {
+  code: function Code({ className, children, ...props }) {
     const isCodeBlock = useIsMarkdownCodeBlock();
     return (
       <code
@@ -210,7 +242,9 @@ const defaultComponents = memoizeMarkdownComponents({
           className,
         )}
         {...props}
-      />
+      >
+        {children}
+      </code>
     );
   },
   CodeHeader,
