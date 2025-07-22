18791c1298800bf7628f73217d81f38cdd48cc56
Randy Shore <randall.shore@gmail.com>
2025-07-22 19:55:28 +0000

    Initial Kion Block for Custom API

---
+++ packages/blocks/kion/src/index.ts
@@ -0,0 +1,38 @@
+import { createCustomApiCallAction } from '@openops/blocks-common';
+import { createBlock, Property } from '@openops/blocks-framework';
+import { BlockCategory } from '@openops/shared';
+import { kionAuth, KionAuth } from './lib/auth';
+
+export const kion = createBlock({
+  displayName: 'Kion',
+  auth: kionAuth,
+  minimumSupportedRelease: '0.20.0',
+  logoUrl: 'https://static.openops.com/blocks/kion.png',
+  authors: [],
+  categories: [BlockCategory.FINOPS],
+  actions: [
+    createCustomApiCallAction({
+      baseUrl: (auth) => {
+        return `${(auth as KionAuth).instanceUrl}/api/`;
+      },
+      auth: kionAuth,
+      additionalProps: {
+        documentation: Property.MarkDown({
+          value:
+            'For more information, visit the [Kion API documentation](https://support.kion.io/hc/en-us/articles/360055160791-App-API-Access).',
+        }),
+      },
+      authMapping: async (context) => {
+        const kionAuth = context.auth as KionAuth;
+        return {
+          Authorization: `Bearer ${kionAuth.apiKey}`,
+          'Content-Type': 'application/json',
+        };
+      },
+      description: 'Make a custom REST API call to Kion',
+      displayName: 'Custom REST API Call',
+      name: 'custom_rest_api_call',
+    }),
+  ],
+  triggers: [],
+});
