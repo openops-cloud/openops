e3853e5ed2236647b5b4d590a6a2a833fbe86ccf
Ravi Kiran <ravikiranvm@gmail.com>
2025-07-22 19:59:01 +0530

    Remove deleteChatRequest function as part of FE cleanup logic

--- packages/react-ui/src/app/features/builder/update-flow-version.ts
+++ packages/react-ui/src/app/features/builder/update-flow-version.ts
@@ -38,7 +38,6 @@ export const updateFlowVersion = (
     if (operation.request.name === state.selectedStep) {
       set({ selectedStep: undefined });
       set({ rightSidebar: RightSideBarType.NONE });
-      deleteChatRequest(state.flowVersion, operation.request.name);
     }
   }
 
@@ -78,25 +77,3 @@ export const updateFlowVersion = (
   flowUpdatesQueue.add(updateRequest);
   return { flowVersion: newFlowVersion };
 };
-
-async function deleteChatRequest(flowVersion: FlowVersion, stepName: string) {
-  try {
-    const stepDetails = flowHelper.getStep(flowVersion, stepName);
-    const blockName = stepDetails?.settings?.blockName;
-    const actionName = stepDetails?.settings?.actionName;
-
-    if (!blockName || !actionName) {
-      return;
-    }
-
-    const chat = await aiChatApi.open(
-      flowVersion.flowId,
-      blockName,
-      stepName,
-      actionName,
-    );
-    await aiChatApi.delete(chat.chatId);
-  } catch (err) {
-    console.error(err);
-  }
-}
