7f34f4733cdbb8f9d64189c3b501da50ed017a1b
Roman Snapko <roman@openops.com>
2025-07-22 15:17:36 +0200

    Add reusable `AssistantUiChatContainer` component (#1001)

---
+++ packages/ui-components/src/components/assistant-ui/assistant-top-bar.tsx
@@ -0,0 +1,62 @@
+import { t } from 'i18next';
+import { SquarePen, X as XIcon } from 'lucide-react';
+import { ReactNode } from 'react';
+import { TooltipWrapper } from '../../components/tooltip-wrapper';
+import { Button } from '../../ui/button';
+
+type AssistantTopBarProps = {
+  onClose: () => void;
+  onNewChat: () => void;
+  enableNewChat: boolean;
+  title?: string;
+  children: ReactNode;
+};
+
+const AssistantTopBar = ({
+  onNewChat,
+  onClose,
+  enableNewChat,
+  title,
+  children,
+}: AssistantTopBarProps) => {
+  return (
+    <div className="flex justify-between items-center px-4 py-2 gap-2 text-md dark:text-primary font-bold border-b border-gray-200">
+      <div className="flex items-center gap-2">
+        <TooltipWrapper tooltipText={t('New chat')}>
+          <Button
+            onClick={(e) => {
+              e.stopPropagation();
+              onNewChat();
+            }}
+            disabled={!enableNewChat}
+            variant="secondary"
+            size="icon"
+            className="text-outline size-[36px]"
+          >
+            <SquarePen size={20} />
+          </Button>
+        </TooltipWrapper>
+        {title}
+      </div>
+      <div className="flex items-center gap-2">
+        {children}
+        <TooltipWrapper tooltipText={t('Close')}>
+          <Button
+            size="icon"
+            variant="basic"
+            onClick={(e) => {
+              e.stopPropagation();
+              onClose();
+            }}
+            className="text-outline size-[36px]"
+          >
+            <XIcon size={20} />
+          </Button>
+        </TooltipWrapper>
+      </div>
+    </div>
+  );
+};
+
+AssistantTopBar.displayName = 'AssistantTopBar';
+export { AssistantTopBar, AssistantTopBarProps };
