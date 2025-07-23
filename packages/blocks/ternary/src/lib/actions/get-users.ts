0614a1ef3c0c9d1d8334fa3f3629818bbb4059e1
Marcelo Gonçalves <marcelo@openops.com>
2025-07-23 11:58:29 +0100

    Fix unit tests

--- packages/blocks/ternary/src/lib/actions/get-users.ts
+++ packages/blocks/ternary/src/lib/actions/get-users.ts
@@ -1,8 +1,7 @@
-import { HttpMethod } from '@openops/blocks-common';
 import { createAction, Property } from '@openops/blocks-framework';
 import { logger } from '@openops/server-shared';
-import { sendTernaryRequest } from '../common';
 import { ternaryCloudAuth } from '../common/auth';
+import { getUsersList } from '../common/users';
 
 export const getUsers = createAction({
   name: 'get_users',
@@ -20,16 +19,7 @@ export const getUsers = createAction({
     const { includeSettings } = propsValue;
 
     try {
-      const response = await sendTernaryRequest({
-        auth: auth,
-        method: HttpMethod.GET,
-        url: 'users',
-        queryParams: {
-          tenantID: auth.tenantId,
-          includeSettings: `${includeSettings ?? false}`,
-        },
-      });
-      return response.body as any[];
+      return await getUsersList(auth, includeSettings);
     } catch (e) {
       logger.error('Error getting users list.', e);
       throw e;
