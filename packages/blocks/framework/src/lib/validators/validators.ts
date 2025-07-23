c1a9ee69c3c695c0b3db154247c44c2016f8d346
Marcelo Gonçalves <marcelo@openops.com>
2025-07-23 17:15:06 +0100

    Add boolean validator to checkbox property (#1018)
    Fixes OPS-2227.

--- packages/blocks/framework/src/lib/validators/validators.ts
+++ packages/blocks/framework/src/lib/validators/validators.ts
@@ -364,6 +364,15 @@ export class Validators {
     },
   };
 
+  static boolean: TypedValidatorFn<ValidationInputType.ANY> = {
+    type: ValidationInputType.ANY,
+    fn: (property, processedValue, userInput) => {
+      return isBoolean(processedValue)
+        ? null
+        : formatErrorMessage(ErrorMessages.BOOLEAN, { userInput });
+    },
+  };
+
   static oneOf(values: unknown[]): TypedValidatorFn<any> {
     return {
       type: ValidationInputType.ANY,
@@ -416,4 +425,13 @@ export class Validators {
       },
     };
   }
+}
+
+function isBoolean(processedValue: unknown): boolean {
+  return (
+    typeof processedValue === 'boolean' ||
+    (typeof processedValue === 'string' &&
+      (processedValue.toLowerCase() === 'true' ||
+        processedValue.toLowerCase() === 'false'))
+  );
 }
