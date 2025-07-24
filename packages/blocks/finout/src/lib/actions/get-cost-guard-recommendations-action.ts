105ae29a37c88454d52419a62c8edb7acf387370
Rita Gorokhod <60586879+rita-gorokhod@users.noreply.github.com>
2025-07-24 13:22:20 +0000

    Add FinOut cost guard action

---
+++ packages/blocks/finout/src/lib/actions/get-cost-guard-recommendations-action.ts
@@ -0,0 +1,45 @@
+import { createAction, Property } from '@openops/blocks-framework';
+import { finoutAuth } from '../auth';
+import { getScanRecommendations, getScans } from '../common/cost-guard';
+
+export const getCostGuardRecommendationsAction = createAction({
+  name: 'finout_get_cost_guard_recommendations',
+  displayName: 'Get Cost Guard Recommendations',
+  description: 'Retrieve Cost Guard Recommendations',
+  auth: finoutAuth,
+  props: {
+    scanId: Property.Dropdown({
+      displayName: 'Scan',
+      description: 'A list of available scans',
+      refreshers: ['auth'],
+      required: true,
+      options: async ({ auth }: any) => {
+        if (!auth) {
+          return {
+            disabled: true,
+            options: [],
+            placeholder: 'Please authenticate first',
+          };
+        }
+
+        const scans = await getScans(auth);
+
+        return {
+          disabled: false,
+          options: scans
+            .map((scan: any) => ({
+              label: scan.name,
+              value: scan.id,
+            }))
+            .sort((a, b) => a.label.localeCompare(b.label)),
+        };
+      },
+    }),
+  },
+  async run(context) {
+    return await getScanRecommendations(
+      context.auth,
+      context.propsValue.scanId,
+    );
+  },
+});
