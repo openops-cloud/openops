1d37bfa683976e908cc18fd4c82ce85d52f83328
BigFluffyCookie <leyla@openops.com>
2025-07-28 12:26:24 +0200

    test

--- packages/blocks/slack/src/lib/actions/request-action-message.ts
+++ packages/blocks/slack/src/lib/actions/request-action-message.ts
@@ -115,10 +115,7 @@ const sendMessageAskingForAction = async (
         {
           queryParams: {
             executionCorrelationId: context.run.pauseId,
-            actionClicked: JSON.stringify({
-              value: action.buttonText,
-              displayText: action.buttonText,
-            }),
+            actionClicked: action.buttonText,
           },
         },
         baseUrl,
