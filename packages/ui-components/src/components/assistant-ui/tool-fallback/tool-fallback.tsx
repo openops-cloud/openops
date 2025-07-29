8957b9491f635e6e0a98b3831d5087edb1f350b1
Roman Snapko <roman@openops.com>
2025-07-29 12:43:44 +0200

    Add tool call status handling and update design (#1041)

--- packages/ui-components/src/components/assistant-ui/tool-fallback/tool-fallback.tsx
+++ packages/ui-components/src/components/assistant-ui/tool-fallback/tool-fallback.tsx
@@ -1,6 +1,11 @@
 import { ToolCallContentPartComponent } from '@assistant-ui/react';
 import { t } from 'i18next';
-import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from 'lucide-react';
+import {
+  ChevronDownIcon,
+  ChevronUpIcon,
+  CircleCheck,
+  XCircle,
+} from 'lucide-react';
 import { useState } from 'react';
 import { Button } from '../../../ui/button';
 
@@ -8,20 +13,34 @@ export const ToolFallback: ToolCallContentPartComponent = ({
   toolName,
   argsText,
   result,
+  status,
 }) => {
   const [isCollapsed, setIsCollapsed] = useState(true);
+
+  const isRunning = status?.type === 'running';
+  const isIncomplete = status?.type === 'incomplete';
+  const isComplete = status?.type === 'complete';
+
   return (
     <div className="mb-4 flex w-full flex-col gap-3 rounded-lg border border-border bg-background py-3">
       <div className="flex items-center gap-2 px-4">
-        <CheckIcon className="size-4 text-foreground" />
+        {isComplete && <CircleCheck className="size-4 text-success" />}
+        {isIncomplete && <XCircle className="size-4 text-foreground" />}
         <p className="text-foreground">
-          {t('Used tool:')} <b className="text-foreground">{toolName}</b>
+          {isComplete && t('Used tool: ')}
+          {isRunning && t('Using tool: ')}
+          {isIncomplete && t('Incomplete tool call: ')}
+          <b className="text-foreground">{toolName}</b>
         </p>
         <div className="flex-grow" />
         <Button
           onClick={() => setIsCollapsed(!isCollapsed)}
           aria-expanded={!isCollapsed}
           aria-controls="collapsible-content"
+          size="icon"
+          variant="default"
+          className={'size-8 p-2 text-white'}
+          loading={isRunning}
         >
           {isCollapsed ? <ChevronDownIcon /> : <ChevronUpIcon />}
         </Button>
