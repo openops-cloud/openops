0f246b8ab9bf275635c7e37a17127646796f04f2
Maor Rozenfeld <49363375+maor-rozenfeld@users.noreply.github.com>
2025-08-06 12:00:57 +0200

    Only add AWS CLI environment variables if they're not empty

--- packages/blocks/aws/src/lib/actions/cli/aws-cli.ts
+++ packages/blocks/aws/src/lib/actions/cli/aws-cli.ts
@@ -6,21 +6,22 @@ export async function runCommand(
   region: string,
   credentials: any,
 ): Promise<string> {
-  if (
-    !credentials.accessKeyId &&
-    !system.getBoolean(SharedSystemProp.AWS_ENABLE_IMPLICIT_ROLE)
-  ) {
+  const envVars: any = {
+    AWS_DEFAULT_REGION: region,
+    PATH: process.env['PATH'] ?? '',
+  };
+
+  if (credentials.accessKeyId && credentials.secretAccessKey) {
+    envVars.AWS_ACCESS_KEY_ID = credentials.accessKeyId;
+    envVars.AWS_SECRET_ACCESS_KEY = credentials.secretAccessKey;
+    if (credentials.sessionToken) {
+      envVars.AWS_SESSION_TOKEN = credentials.sessionToken;
+    }
+  } else if (!system.getBoolean(SharedSystemProp.AWS_ENABLE_IMPLICIT_ROLE)) {
     throw new Error(
       'AWS credentials are required, please provide accessKeyId and secretAccessKey',
     );
   }
-  const envVars = {
-    AWS_ACCESS_KEY_ID: credentials.accessKeyId,
-    AWS_SECRET_ACCESS_KEY: credentials.secretAccessKey,
-    AWS_SESSION_TOKEN: credentials.sessionToken,
-    AWS_DEFAULT_REGION: region,
-    PATH: process.env['PATH'] ?? '',
-  };
 
   return await runCliCommand(command, 'aws', envVars);
 }
