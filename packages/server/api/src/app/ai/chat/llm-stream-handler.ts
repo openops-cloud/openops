fce49a15fb759aa49a530b155b4e3ed673f2951a
Marcelo Gonçalves <marcelo@openops.com>
2025-08-08 17:12:38 +0100

    Revert

--- packages/server/api/src/app/ai/chat/llm-stream-handler.ts
+++ packages/server/api/src/app/ai/chat/llm-stream-handler.ts
@@ -1,4 +1,3 @@
-import { isLLMTelemetryEnabled } from '@openops/common';
 import { AiConfig } from '@openops/shared';
 import {
   CoreMessage,
@@ -9,7 +8,6 @@ import {
   TextStreamPart,
   ToolSet,
 } from 'ai';
-import { gpt5SchemaAdapter } from './adapters';
 
 type AICallSettings = {
   tools?: ToolSet;
@@ -42,42 +40,20 @@ export function getLLMAsyncStream(
   const hasTools = tools && Object.keys(tools).length !== 0;
   const toolChoice = hasTools ? 'auto' : 'none';
 
-  const updatedTools: ToolSet = {};
-  if (tools) {
-    for (const [toolName, tool] of Object.entries(tools)) {
-      const schema = tool.parameters.jsonSchema;
-
-      const shouldUseSchemaAdapter = languageModel.modelId.includes('gpt-5');
-      const fixedSchema = shouldUseSchemaAdapter
-        ? gpt5SchemaAdapter(schema as Record<string, unknown>)
-        : schema;
-
-      updatedTools[toolName] = {
-        ...tool,
-        parameters: {
-          ...tool.parameters,
-          jsonSchema: fixedSchema,
-        },
-      };
-    }
-  }
-
   const { fullStream } = streamText({
     model: languageModel,
     system: systemPrompt,
     messages: chatHistory,
     ...aiConfig.modelSettings,
-    tools: updatedTools,
+    tools,
     toolChoice,
     maxRetries: MAX_RETRIES,
     maxSteps: maxRecursionDepth,
+    toolCallStreaming: true,
     onStepFinish,
     onFinish,
     async onError({ error }): Promise<void> {
       throw error;
-    },
-    experimental_telemetry: {
-      isEnabled: isLLMTelemetryEnabled(),
     },
   });
 
