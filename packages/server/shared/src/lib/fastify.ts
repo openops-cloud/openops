9a13b90574492c197d6a1717b11930d6efae7221
Marcelo Gonçalves <marcelo@openops.com>
2025-08-13 16:07:43 +0100

    Simplify code

--- packages/server/shared/src/lib/fastify.ts
+++ packages/server/shared/src/lib/fastify.ts
@@ -3,26 +3,6 @@ import { AppSystemProp, system } from './system';
 
 export const ONE_MB_IN_BYTES = 1024 * 1024;
 
-export const getMaximumRequestBodySizeInBytes = (): number => {
-  const bodyLimitInMb = Number.parseInt(
-    system.getOrThrow<string>(AppSystemProp.REQUEST_BODY_LIMIT),
-  );
-
-  if (Number.isNaN(bodyLimitInMb)) {
-    throw new ApplicationError(
-      {
-        code: ErrorCode.SYSTEM_PROP_INVALID,
-        params: {
-          prop: AppSystemProp.REQUEST_BODY_LIMIT,
-        },
-      },
-      `System property OPS_${AppSystemProp.REQUEST_BODY_LIMIT} is not a valid integer`,
-    );
-  }
-
-  return ONE_MB_IN_BYTES * bodyLimitInMb;
-};
-
 export const getMaximumRequestBodySizeInMegabytes = (): number => {
   const bodyLimitInMb = Number.parseInt(
     system.getOrThrow<string>(AppSystemProp.REQUEST_BODY_LIMIT),
@@ -41,6 +21,10 @@ export const getMaximumRequestBodySizeInMegabytes = (): number => {
   }
 
   return bodyLimitInMb;
+};
+
+export const getMaximumRequestBodySizeInBytes = (): number => {
+  return getMaximumRequestBodySizeInMegabytes() * ONE_MB_IN_BYTES;
 };
 
 export const getMaximumRequestBodySizeWithBufferInMegabytes = (): number => {
