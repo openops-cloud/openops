1deb2933353bb6b8f4c73e340837fc7f220ed79e
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2025-08-13 11:47:29 +0300

    mark ui kit as external in vite

--- packages/react-ui/vite.config.ts
+++ packages/react-ui/vite.config.ts
@@ -70,6 +70,7 @@ export default defineConfig({
       transformMixedEsModules: true,
     },
     rollupOptions: {
+      external: ['@openops/ui-kit'],
       onLog(level, log, handler) {
         if (
           log.cause &&
