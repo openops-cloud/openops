58d102873c4620b678b5048047dd3ba979f6bcc5
Roman Snapko <roman@openops.com>
2025-08-13 16:17:42 +0200

    feat: add auto-instances shutdown table seeding functionality

---
+++ packages/server/api/src/app/openops-tables/template-tables/create-auto-instances-shutdown-table.ts
@@ -0,0 +1,87 @@
+import {
+  createAxiosHeaders,
+  getFields,
+  getPrimaryKeyFieldFromFields,
+  makeOpenOpsTablesPatch,
+  makeOpenOpsTablesPost,
+} from '@openops/common';
+import { logger } from '@openops/server-shared';
+import { createTable } from '../create-table';
+
+export const SEED_OPENOPS_AUTO_INSTANCES_SHUTDOWN_TABLE_NAME =
+  'Auto instances shutdown';
+
+export async function createAutoInstancesShutdownTable(
+  token: string,
+  databaseId: number,
+) {
+  logger.debug(
+    `[Seeding ${SEED_OPENOPS_AUTO_INSTANCES_SHUTDOWN_TABLE_NAME} table] Start`,
+  );
+
+  const table = await createTable(
+    databaseId,
+    SEED_OPENOPS_AUTO_INSTANCES_SHUTDOWN_TABLE_NAME,
+    [['Resource ID']],
+    token,
+  );
+
+  const fields = await getFields(table.id, token);
+  const primaryField = getPrimaryKeyFieldFromFields(fields);
+
+  logger.debug(
+    `[Seeding ${SEED_OPENOPS_AUTO_INSTANCES_SHUTDOWN_TABLE_NAME} table] Before adding primary field ID with id: ${primaryField.id}`,
+  );
+  await makeOpenOpsTablesPatch<unknown>(
+    `api/database/fields/${primaryField.id}/`,
+    {
+      name: 'Resource ID',
+      type: 'text',
+    },
+    createAxiosHeaders(token),
+  );
+  logger.debug(
+    `[Seeding ${SEED_OPENOPS_AUTO_INSTANCES_SHUTDOWN_TABLE_NAME} table] After adding primary field ID with id: ${primaryField.id}`,
+  );
+
+  await addField(token, table.id, {
+    name: 'Shutdown time',
+    type: 'date',
+    date_format: 'ISO',
+    date_include_time: true,
+  });
+
+  const fieldNames = ['Cloud provider', 'Workflow', 'Status'];
+
+  for (const fieldName of fieldNames) {
+    await addField(token, table.id, { name: fieldName, type: 'text' });
+  }
+
+  logger.debug(
+    `[Seeding ${SEED_OPENOPS_AUTO_INSTANCES_SHUTDOWN_TABLE_NAME} table] Done`,
+  );
+}
+
+async function addField(
+  token: string,
+  tableId: number,
+  fieldBody: Record<string, unknown>,
+): Promise<{ id: number }> {
+  const createFieldEndpoint = `api/database/fields/table/${tableId}/`;
+
+  logger.debug(
+    `[Seeding ${SEED_OPENOPS_AUTO_INSTANCES_SHUTDOWN_TABLE_NAME} Before adding field ${fieldBody.name}`,
+  );
+
+  const field = await makeOpenOpsTablesPost<{ id: number }>(
+    createFieldEndpoint,
+    fieldBody,
+    createAxiosHeaders(token),
+  );
+
+  logger.debug(
+    `[Seeding  ${SEED_OPENOPS_AUTO_INSTANCES_SHUTDOWN_TABLE_NAME} table] After adding field ${fieldBody.name} with id: ${field.id}`,
+  );
+
+  return field;
+}
