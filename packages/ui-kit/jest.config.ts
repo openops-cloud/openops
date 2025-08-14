2c44e0440cf65b3f42632128a3e8584c77f23c68
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2025-08-14 10:31:54 +0300

    Add stub ui-kit and update packages (#1141)

---
+++ packages/ui-kit/jest.config.ts
@@ -0,0 +1,16 @@
+export default {
+  displayName: 'ui-kit',
+  preset: '../../jest.preset.js',
+  setupFiles: ['../../jest.env.js', './setup-tests.ts'],
+  transformIgnorePatterns: ['node_modules/(?!(lodash-es|remark-gfm)/)'],
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/packages/ui-kit',
+  moduleNameMapper: {
+    '^@assistant-ui/(.*)$': '<rootDir>/src/__mocks__/assistant-ui/$1',
+    '^remark-gfm$': '<rootDir>/src/__mocks__/remark-gfm.ts',
+  },
+};
