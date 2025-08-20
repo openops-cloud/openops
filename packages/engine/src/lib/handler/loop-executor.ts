05f09b665e63266f9cd1ca242c2379354ec56dba
Marcelo Gonçalves <marcelo@openops.com>
2025-08-20 09:26:17 +0100

    Remove import

--- packages/engine/src/lib/handler/loop-executor.ts
+++ packages/engine/src/lib/handler/loop-executor.ts
@@ -12,7 +12,6 @@ import {
 } from '@openops/shared';
 import cloneDeep from 'lodash.clonedeep';
 import { nanoid } from 'nanoid';
-import { isSizeValidationError } from '../helper/size-validation';
 import { createContextStore } from '../services/storage.service';
 import { BaseExecutor } from './base-executor';
 import { EngineConstants } from './context/engine-constants';
