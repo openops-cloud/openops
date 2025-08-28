73e8132c7d1b7d3faf668dd4393cd006ef3351d5
Rita Gorokhod <60586879+rita-gorokhod@users.noreply.github.com>
2025-08-28 11:28:42 +0000

    Fix tests

--- packages/blocks/vantage/test/index.test.ts
+++ packages/blocks/vantage/test/index.test.ts
@@ -13,10 +13,18 @@ describe('block declaration tests', () => {
   });
 
   test('should return block with correct number of actions', () => {
-    expect(Object.keys(vantage.actions()).length).toBe(1);
+    expect(Object.keys(vantage.actions()).length).toBe(3);
     expect(vantage.actions()).toMatchObject({
       custom_api_call: {
         name: 'custom_api_call',
+        requireAuth: true,
+      },
+      vantage_get_integrations: {
+        name: 'vantage_get_integrations',
+        requireAuth: true,
+      },
+      vantage_get_active_recommendations: {
+        name: 'vantage_get_active_recommendations',
         requireAuth: true,
       },
     });
