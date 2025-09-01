cc418b64ec5e2ba1b7e804b559a3f1443e450726
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2025-09-01 13:56:09 +0300

    Builder socket updates (#1222)

---
+++ packages/react-ui/src/app/features/builder/hooks/use-flow-updates.ts
@@ -0,0 +1,80 @@
+import { useQueryClient } from '@tanstack/react-query';
+import { useEffect } from 'react';
+
+import { useSocket } from '@/app/common/providers/socket-provider';
+import { QueryKeys } from '@/app/constants/query-keys';
+import {
+  WebsocketClientEvent,
+  WorkflowStepAddedPayload,
+  WorkflowStepTestedPayload,
+  WorkflowStepUpdatedPayload,
+} from '@openops/shared';
+import { useParams } from 'react-router-dom';
+import { useBuilderStateContext } from '../builder-hooks';
+import { stepTestOutputCache } from '../data-selector/data-selector-cache';
+
+export const useFlowUpdates = () => {
+  const { flowId } = useParams();
+  const socket = useSocket();
+  const queryClient = useQueryClient();
+  const refreshSettings = useBuilderStateContext(
+    (state) => state.refreshSettings,
+  );
+
+  useEffect(() => {
+    if (!flowId) {
+      return;
+    }
+
+    const handleUpdate = (
+      payload:
+        | WorkflowStepAddedPayload
+        | WorkflowStepUpdatedPayload
+        | WorkflowStepTestedPayload,
+    ) => {
+      if (payload.flowId === flowId) {
+        queryClient.invalidateQueries({
+          queryKey: [QueryKeys.flow, flowId],
+        });
+      }
+
+      if ('stepId' in payload && payload.stepId) {
+        stepTestOutputCache.clearStep(payload.stepId);
+        queryClient.invalidateQueries({
+          queryKey: [
+            QueryKeys.stepTestOutput,
+            payload.flowVersionId,
+            payload.stepId,
+          ],
+        });
+      }
+    };
+
+    socket.on(WebsocketClientEvent.WORKFLOW_STEP_ADDED, handleUpdate);
+    socket.on(WebsocketClientEvent.WORKFLOW_STEP_UPDATED, handleUpdate);
+    socket.on(WebsocketClientEvent.WORKFLOW_STEP_TESTED, handleUpdate);
+
+    return () => {
+      socket.removeAllListeners(WebsocketClientEvent.WORKFLOW_STEP_ADDED);
+      socket.removeAllListeners(WebsocketClientEvent.WORKFLOW_STEP_UPDATED);
+      socket.removeAllListeners(WebsocketClientEvent.WORKFLOW_STEP_TESTED);
+    };
+  }, [socket, queryClient, flowId, refreshSettings]);
+
+  useEffect(() => {
+    // update step settings after the flow query
+    const unsubscribe = queryClient.getQueryCache().subscribe((event) => {
+      if (
+        event?.query.state.status === 'success' &&
+        event?.query.queryKey[0] === QueryKeys.flow &&
+        event?.query.queryKey[1] === flowId
+      ) {
+        requestAnimationFrame(() => {
+          refreshSettings();
+        });
+      }
+    });
+
+    return unsubscribe;
+  }, [flowId, queryClient, refreshSettings]);
+};
