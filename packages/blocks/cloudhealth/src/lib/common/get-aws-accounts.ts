722da09af394a9dc073e27fcf2623936acfc4dff
Rita Gorokhod <60586879+rita-gorokhod@users.noreply.github.com>
2025-09-15 14:25:08 +0000

    Paginate CH Get AWS accounts response

--- packages/blocks/cloudhealth/src/lib/common/get-aws-accounts.ts
+++ packages/blocks/cloudhealth/src/lib/common/get-aws-accounts.ts
@@ -1,8 +1,29 @@
 import { makeGetRequest } from './call-rest-api';
 
 export async function getAwsAccounts(apiKey: string): Promise<unknown[]> {
-  return (
-    ((await makeGetRequest(apiKey, '/v1/aws_accounts')) as any).aws_accounts ||
-    []
-  );
+  let page = 1;
+  let allAccounts: unknown[] = [];
+  let paginate = true;
+
+  const perPage = 100;
+
+  while (paginate) {
+    const response = await makeGetRequest(apiKey, `/api/aws_account`, {
+      page: page.toString(),
+      per_page: perPage.toString(),
+    });
+
+    const accounts = response.body.aws_accounts || [];
+    allAccounts = allAccounts.concat(accounts);
+
+    const total = Number(response.headers?.['x-total'] ?? 0);
+
+    if (allAccounts.length < total) {
+      page++;
+    } else {
+      paginate = false;
+    }
+  }
+
+  return allAccounts;
 }
