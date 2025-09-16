b20ee0cf892865871e4427b19c7071a4a58cea87
BigFluffyCookie <leyla@openops.com>
2025-09-16 17:16:26 +0200

    Add azure openai support

--- packages/openops/src/lib/ai/providers/azure-openai.ts
+++ packages/openops/src/lib/ai/providers/azure-openai.ts
@@ -1,12 +1,30 @@
 import { createAzure } from '@ai-sdk/azure';
+import { logger } from '@openops/server-shared';
 import { LanguageModel } from 'ai';
 import { AiProvider } from '../providers';
+
+function isApimUrl(url?: string) {
+  return !!url && url.includes('.azure-api.net');
+}
 
 function createLanguageModel(params: {
   apiKey: string;
   model: string;
   providerSettings?: Record<string, unknown>;
 }): LanguageModel {
+  const isApimMode = isApimUrl(params.providerSettings?.['baseURL'] as string);
+  if (isApimMode) {
+    return createAzure({
+      apiKey: '',
+      headers: {
+        'Ocp-Apim-Subscription-Key': params.apiKey,
+      },
+      useDeploymentBasedUrls: true,
+      apiVersion: '2024-02-01',
+      ...params.providerSettings,
+    })(params.model);
+  }
+
   return createAzure({
     apiKey: params.apiKey,
     ...params.providerSettings,
