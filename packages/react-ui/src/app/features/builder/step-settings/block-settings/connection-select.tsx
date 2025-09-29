05add840627ea4114bde060307916cc9dbe1cd7c
Roman Snapko <roman@openops.com>
2025-09-29 10:43:18 +0200

    chore: Update connections select to work outside step settings

--- packages/react-ui/src/app/features/builder/step-settings/block-settings/connection-select.tsx
+++ packages/react-ui/src/app/features/builder/step-settings/block-settings/connection-select.tsx
@@ -15,8 +15,6 @@ import {
 } from '@openops/components/ui';
 import {
   addConnectionBrackets,
-  BlockAction,
-  BlockTrigger,
   removeConnectionBrackets,
 } from '@openops/shared';
 import { t } from 'i18next';
@@ -32,184 +30,195 @@ import {
   appConnectionsHooks,
   FETCH_ALL_CONNECTIONS_LIMIT,
 } from '@/app/features/connections/lib/app-connections-hooks';
-import { useBuilderStateContext } from '../../builder-hooks';
+import { useSafeBuilderStateContext } from '../../builder-hooks';
 
 type ConnectionSelectProps = {
   disabled: boolean;
   block: BlockMetadataModelSummary | BlockMetadataModel;
-  isTrigger: boolean;
+  allowDynamicValues: boolean;
   providerKey: string;
+  name: string;
 };
 
-const ConnectionSelect = memo((params: ConnectionSelectProps) => {
-  const [connectionDialogOpen, setConnectionDialogOpen] = useState(false);
-  const [selectConnectionOpen, setSelectConnectionOpen] = useState(false);
-  const [reconnectConnectionId, setReconnectConnectionId] = useState<
-    string | null
-  >(null);
+const ConnectionSelect = memo(
+  ({
+    disabled,
+    block,
+    allowDynamicValues,
+    providerKey,
+    name,
+  }: ConnectionSelectProps) => {
+    const [connectionDialogOpen, setConnectionDialogOpen] = useState(false);
+    const [selectConnectionOpen, setSelectConnectionOpen] = useState(false);
+    const [reconnectConnectionId, setReconnectConnectionId] = useState<
+      string | null
+    >(null);
 
-  const form = useFormContext<BlockAction | BlockTrigger>();
-  const {
-    data: connectionsPage,
-    isLoading,
-    refetch,
-  } = appConnectionsHooks.useConnections({
-    authProviders: [params.providerKey],
-    cursor: undefined,
-    limit: FETCH_ALL_CONNECTIONS_LIMIT,
-  });
-
-  const { data: reconnectConnection, isLoading: isLoadingConnection } =
-    appConnectionsHooks.useConnection({
-      id: reconnectConnectionId,
+    const form = useFormContext();
+    const {
+      data: connectionsPage,
+      isLoading,
+      refetch,
+    } = appConnectionsHooks.useConnections({
+      authProviders: [providerKey],
+      cursor: undefined,
+      limit: FETCH_ALL_CONNECTIONS_LIMIT,
     });
 
-  const [refreshDynamicProperties] = useBuilderStateContext((state) => [
-    state.refreshDynamicPropertiesForAuth,
-  ]);
+    const { data: reconnectConnection, isLoading: isLoadingConnection } =
+      appConnectionsHooks.useConnection({
+        id: reconnectConnectionId,
+      });
 
-  const handleReconnectClick = useCallback(
-    (e: React.MouseEvent<HTMLButtonElement>) => {
-      e.stopPropagation();
-      const connectionName = removeConnectionBrackets(
-        form.getValues().settings.input.auth ?? '',
-      );
+    const refreshDynamicProperties = useSafeBuilderStateContext(
+      (state) => state?.refreshDynamicPropertiesForAuth,
+    );
 
-      const matchedConnectionId =
-        connectionsPage?.data?.find((c) => c.name === connectionName)?.id ??
-        null;
+    const handleReconnectClick = useCallback(
+      (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
+        const currentValue = form.getValues(name);
+        const connectionName = removeConnectionBrackets(currentValue ?? '');
 
-      setReconnectConnectionId(matchedConnectionId);
-      setSelectConnectionOpen(false);
-      setConnectionDialogOpen(true);
-    },
-    [connectionsPage?.data, form],
-  );
+        const matchedConnectionId =
+          connectionsPage?.data?.find((c) => c.name === connectionName)?.id ??
+          null;
 
-  return (
-    <FormField
-      control={form.control}
-      name={'settings.input.auth'}
-      render={({ field }) => (
-        <>
-          {isLoading && (
-            <Select disabled={params.disabled}>
-              <SelectContent>
-                <SelectLoader />
-              </SelectContent>
-            </Select>
-          )}
-          {!isLoading && (
-            <AutoFormFieldWrapper
-              property={params.block.auth!}
-              propertyName="auth"
-              field={field as unknown as ControllerRenderProps}
-              disabled={params.disabled}
-              hideDescription={true}
-              inputName="settings.input.auth"
-              allowDynamicValues={!params.isTrigger}
-            >
-              {connectionDialogOpen && !isLoadingConnection && (
-                <DynamicFormValidationProvider>
-                  <CreateOrEditConnectionDialog
-                    connectionToEdit={reconnectConnection ?? null}
-                    reconnect={true}
-                    key={reconnectConnection?.name || 'newConnection'}
-                    authProviderKey={params.providerKey}
-                    onConnectionSaved={async (connectionName) => {
-                      await refetch();
-                      field.onChange(addConnectionBrackets(connectionName));
-                      refreshDynamicProperties();
-                    }}
-                    open={connectionDialogOpen}
-                    setOpen={setConnectionDialogOpen}
-                  ></CreateOrEditConnectionDialog>
-                </DynamicFormValidationProvider>
-              )}
+        setReconnectConnectionId(matchedConnectionId);
+        setSelectConnectionOpen(false);
+        setConnectionDialogOpen(true);
+      },
+      [connectionsPage?.data, form, name],
+    );
 
-              <Select
-                open={selectConnectionOpen}
-                onOpenChange={setSelectConnectionOpen}
-                defaultValue={(field.value as string) ?? undefined}
-                value={field.value ?? undefined}
-                onValueChange={(v) => {
-                  if (v && v !== field.value) {
-                    field.onChange(v);
-                  }
-                }}
-                disabled={params.disabled}
+    return (
+      <FormField
+        control={form.control}
+        name={name}
+        render={({ field }) => (
+          <>
+            {isLoading && (
+              <Select disabled={disabled}>
+                <SelectContent>
+                  <SelectLoader />
+                </SelectContent>
+              </Select>
+            )}
+            {!isLoading && (
+              <AutoFormFieldWrapper
+                property={block.auth!}
+                propertyName="auth"
+                field={field as unknown as ControllerRenderProps}
+                disabled={disabled}
+                hideDescription={true}
+                inputName={name}
+                allowDynamicValues={allowDynamicValues}
               >
-                <div className="relative">
-                  {field.value && !field.disabled && (
-                    <Button
-                      variant="ghost"
-                      size="xs"
-                      className="z-50 absolute right-8 top-2 "
-                      onClick={handleReconnectClick}
-                    >
-                      {t('Reconnect')}
-                    </Button>
-                  )}
+                {connectionDialogOpen && !isLoadingConnection && (
+                  <DynamicFormValidationProvider>
+                    <CreateOrEditConnectionDialog
+                      connectionToEdit={reconnectConnection ?? null}
+                      reconnect={true}
+                      key={reconnectConnection?.name || 'newConnection'}
+                      authProviderKey={providerKey}
+                      onConnectionSaved={async (connectionName) => {
+                        await refetch();
+                        field.onChange(addConnectionBrackets(connectionName));
+                        refreshDynamicProperties?.();
+                      }}
+                      open={connectionDialogOpen}
+                      setOpen={setConnectionDialogOpen}
+                    ></CreateOrEditConnectionDialog>
+                  </DynamicFormValidationProvider>
+                )}
 
-                  <SelectTrigger className="flex gap-2 items-center">
-                    <SelectValue
-                      className="truncate flex-grow flex-shrink"
-                      placeholder={t('Select a connection')}
-                    >
-                      {!!field.value && (
-                        <div className="truncate flex-grow flex-shrink">
-                          {removeConnectionBrackets(field.value)}
-                        </div>
-                      )}
-                    </SelectValue>
-                    <div className="grow"></div>
-                    {/* Hidden Button to take same space as shown button */}
-                    {field.value && (
+                <Select
+                  open={selectConnectionOpen}
+                  onOpenChange={setSelectConnectionOpen}
+                  defaultValue={(field.value as string) ?? undefined}
+                  value={field.value ?? undefined}
+                  onValueChange={(v) => {
+                    if (v && v !== field.value) {
+                      field.onChange(v);
+                    }
+                  }}
+                  disabled={disabled}
+                >
+                  <div className="relative">
+                    {field.value && !field.disabled && (
                       <Button
+                        type="button"
                         variant="ghost"
                         size="xs"
-                        className="z-50 opacity-0 pointer-events-none"
+                        className="z-50 absolute right-8 top-2 "
+                        onClick={handleReconnectClick}
                       >
                         {t('Reconnect')}
                       </Button>
                     )}
-                  </SelectTrigger>
-                </div>
 
-                <SelectContent>
-                  <SelectAction
-                    onClick={() => {
-                      setReconnectConnectionId(null);
-                      setSelectConnectionOpen(false);
-                      setConnectionDialogOpen(true);
-                    }}
-                  >
-                    <span className="flex items-center gap-1 text-primary w-full">
-                      <Plus size={16} />
-                      {t('Create Connection')}
-                    </span>
-                  </SelectAction>
-
-                  {connectionsPage?.data &&
-                    connectionsPage.data.map((connection) => {
-                      return (
-                        <SelectItem
-                          value={addConnectionBrackets(connection.name)}
-                          key={connection.name}
+                    <SelectTrigger className="flex gap-2 items-center">
+                      <SelectValue
+                        className="truncate flex-grow flex-shrink"
+                        placeholder={t('Select a connection')}
+                      >
+                        {!!field.value && (
+                          <div className="truncate flex-grow flex-shrink">
+                            {removeConnectionBrackets(field.value)}
+                          </div>
+                        )}
+                      </SelectValue>
+                      <div className="grow"></div>
+                      {/* Hidden Button to take same space as shown button */}
+                      {field.value && (
+                        <Button
+                          type="button"
+                          variant="ghost"
+                          size="xs"
+                          className="z-50 opacity-0 pointer-events-none"
                         >
-                          {connection.name}
-                        </SelectItem>
-                      );
-                    })}
-                </SelectContent>
-              </Select>
-            </AutoFormFieldWrapper>
-          )}
-        </>
-      )}
-    ></FormField>
-  );
-});
+                          {t('Reconnect')}
+                        </Button>
+                      )}
+                    </SelectTrigger>
+                  </div>
+
+                  <SelectContent>
+                    <SelectAction
+                      onClick={() => {
+                        setReconnectConnectionId(null);
+                        setSelectConnectionOpen(false);
+                        setConnectionDialogOpen(true);
+                      }}
+                    >
+                      <span className="flex items-center gap-1 text-primary w-full">
+                        <Plus size={16} />
+                        {t('Create Connection')}
+                      </span>
+                    </SelectAction>
+
+                    {connectionsPage?.data &&
+                      connectionsPage.data.map((connection) => {
+                        return (
+                          <SelectItem
+                            value={addConnectionBrackets(connection.name)}
+                            key={connection.name}
+                          >
+                            {connection.name}
+                          </SelectItem>
+                        );
+                      })}
+                  </SelectContent>
+                </Select>
+              </AutoFormFieldWrapper>
+            )}
+          </>
+        )}
+      ></FormField>
+    );
+  },
+);
 
 ConnectionSelect.displayName = 'ConnectionSelect';
 export { ConnectionSelect };
