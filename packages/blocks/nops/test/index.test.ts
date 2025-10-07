ce0073c3e1deff3f327903b8592d8ac6353454ab
Andre Leite <andre@nops.io>
2025-10-07 09:35:02 -0300

    linting issue

--- packages/blocks/nops/test/index.test.ts
+++ packages/blocks/nops/test/index.test.ts
@@ -32,7 +32,9 @@ describe('block declaration tests', () => {
   test('should have cost summary action configured', () => {
     const actions = nops.actions();
     expect(actions['nops_get_cost_summary']).toBeDefined();
-    expect(actions['nops_get_cost_summary'].displayName).toBe('Get Cost Summary');
+    expect(actions['nops_get_cost_summary'].displayName).toBe(
+      'Get Cost Summary',
+    );
   });
 
   test('should have organization accounts action configured', () => {
