f30867262b126369b4450926f87124df67319e54
Roman Snapko <roman@openops.com>
2025-10-10 12:41:38 +0200

    Add CommunityBadge to menu header and general settings page (#1427)
    <!--
    Ensure the title clearly reflects what was changed.
    Provide a clear and concise description of the changes made. The PR
    should only contain the changes related to the issue, and no other
    unrelated changes.
    -->
    
    Fixes OPS-2707.
    
    
    https://www.loom.com/share/71b5f7da7d6149268b3cae23e34847e3?sid=ce23a536-208d-4b86-bb41-2cde9f5f6a95

--- packages/react-ui/src/app/features/navigation/side-menu/dashboard/dashboard-side-menu-header.tsx
+++ packages/react-ui/src/app/features/navigation/side-menu/dashboard/dashboard-side-menu-header.tsx
@@ -1,11 +1,14 @@
 import { AppLogo } from '@/app/common/components/app-logo';
+import { flagsHooks } from '@/app/common/hooks/flags-hooks';
 import { useAppStore } from '@/app/store/app-store';
 import {
   Button,
   cn,
+  CommunityBadge,
   SideMenuHeader,
   TooltipWrapper,
 } from '@openops/components/ui';
+import { FlagId } from '@openops/shared';
 import { t } from 'i18next';
 import { PanelRight } from 'lucide-react';
 import { useCallback } from 'react';
@@ -16,32 +19,53 @@ const DashboardSideMenuHeader = () => {
     setIsSidebarMinimized: state.setIsSidebarMinimized,
   }));
 
+  const showCommunity = flagsHooks.useFlag(FlagId.SHOW_COMMUNITY)
+    .data as boolean;
+
   const toggleSidebar = useCallback(() => {
     setIsSidebarMinimized(!isMinimized);
   }, [isMinimized, setIsSidebarMinimized]);
 
   const tooltipText = isMinimized ? t('Open sidebar') : t('Close sidebar');
 
+  const showCommunityBadge = !isMinimized && showCommunity;
+
   return (
     <SideMenuHeader
-      className={cn('justify-between overflow-hidden', {
-        'justify-start pl-3': isMinimized,
+      className={cn('justify-between gap-1 overflow-hidden px-4', {
+        'justify-start pl-3 gap-0': isMinimized,
       })}
       logo={isMinimized ? null : <AppLogo className="h-6" />}
     >
-      <TooltipWrapper tooltipText={tooltipText} tooltipPlacement="right">
-        <Button
-          variant="ghost"
-          size="sm"
-          onClick={toggleSidebar}
-          aria-label={tooltipText}
-          className={cn({
-            'mr-4': isMinimized,
-          })}
-        >
-          <PanelRight className="animate-fade size-[18px]" strokeWidth="2.3" />
-        </Button>
-      </TooltipWrapper>
+      <div
+        className={cn('flex-1 flex items-center justify-between', {
+          'justify-end': !showCommunityBadge,
+        })}
+      >
+        {showCommunityBadge && (
+          <CommunityBadge
+            showUpgrade={false}
+            link={'https://www.openops.com/pricing/'}
+          />
+        )}
+
+        <TooltipWrapper tooltipText={tooltipText} tooltipPlacement="right">
+          <Button
+            variant="ghost"
+            size="sm"
+            onClick={toggleSidebar}
+            aria-label={tooltipText}
+            className={cn({
+              'mr-4': isMinimized,
+            })}
+          >
+            <PanelRight
+              className="animate-fade size-[18px]"
+              strokeWidth="2.3"
+            />
+          </Button>
+        </TooltipWrapper>
+      </div>
     </SideMenuHeader>
   );
 };
