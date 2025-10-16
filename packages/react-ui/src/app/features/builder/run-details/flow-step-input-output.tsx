db98d0f941dc1b486aee513a2f752b7638e4ce19
Roman Snapko <roman@openops.com>
2025-10-16 18:13:35 +0200

    Add conditional display for `TestRunActionLimitReachedPlate` based on step status

--- packages/react-ui/src/app/features/builder/run-details/flow-step-input-output.tsx
+++ packages/react-ui/src/app/features/builder/run-details/flow-step-input-output.tsx
@@ -1,15 +1,20 @@
 import { TestStepDataViewer } from '@openops/components/ui';
 import { t } from 'i18next';
 import { Timer } from 'lucide-react';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { flagsHooks } from '@/app/common/hooks/flags-hooks';
 import { useTheme } from '@/app/common/providers/theme-provider';
 import { useBuilderStateContext } from '@/app/features/builder/builder-hooks';
-import { TestRunActionLimitReachedPlate } from '@/app/features/builder/run-details/test-run-action-limit-reached-plate';
 import { StepStatusIcon } from '@/app/features/flow-runs/components/step-status-icon';
 import { formatUtils } from '@/app/lib/utils';
-import { FlagId, flowHelper, StepOutput } from '@openops/shared';
+import {
+  FlagId,
+  flowHelper,
+  StepOutput,
+  StepOutputStatus,
+} from '@openops/shared';
+import { TestRunActionLimitReachedPlate } from './test-run-action-limit-reached-plate';
 
 const FlowStepInputOutput = React.memo(
   ({ stepDetails }: { stepDetails: StepOutput }) => {
@@ -29,15 +34,37 @@ const FlowStepInputOutput = React.memo(
       ? flowHelper.getStep(flowVersion, selectedStepName)
       : undefined;
 
+    const testRunActionLimitReachedMessage = useMemo(() => {
+      if (stepDetails.status !== StepOutputStatus.TEST_RUN_LIMIT_REACHED) {
+        return '';
+      }
+
+      try {
+        return JSON.parse(stepDetails.errorMessage ?? '').message;
+      } catch {
+        return t('Action limit reached.');
+      }
+    }, [stepDetails.errorMessage, stepDetails.status]);
+
     const { theme } = useTheme();
     if (!stepDetails) {
       return null;
     }
     return (
       <div className="h-full flex flex-col gap-2 p-4">
-        <div className="flex items-center gap-2 justify-start mb-4">
-          <StepStatusIcon status={stepDetails.status} size="5"></StepStatusIcon>
-          <div>{selectedStep?.displayName}</div>
+        <div>
+          <div className="flex items-center gap-2 justify-start mb-4">
+            <StepStatusIcon
+              status={stepDetails.status}
+              size="5"
+            ></StepStatusIcon>
+            <div>{selectedStep?.displayName}</div>
+          </div>
+          {testRunActionLimitReachedMessage && (
+            <TestRunActionLimitReachedPlate
+              error={testRunActionLimitReachedMessage}
+            />
+          )}
         </div>
         <div className="flex items-center gap-1 justify-start">
           {durationEnabled && (
@@ -51,9 +78,6 @@ const FlowStepInputOutput = React.memo(
             </>
           )}
         </div>
-        <TestRunActionLimitReachedPlate
-          error={'Action limit reached: N runs'}
-        />
         <TestStepDataViewer
           inputJson={stepDetails.input}
           outputJson={stepOutput}
