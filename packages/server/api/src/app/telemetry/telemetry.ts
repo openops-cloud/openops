6577f4f786751c0ffa7e86d2af5452374f554930
Marcelo Gonçalves <marcelo@openops.com>
2025-10-23 11:00:36 +0100

    Update packages/server/api/src/app/telemetry/telemetry.ts
    Co-authored-by: Maor Rozenfeld <49363375+maor-rozenfeld@users.noreply.github.com>

--- packages/server/api/src/app/telemetry/telemetry.ts
+++ packages/server/api/src/app/telemetry/telemetry.ts
@@ -30,7 +30,7 @@ export const telemetry = {
   async start(getEnvironmentId: () => Promise<UUID>): Promise<void> {
     if (!isTelemetryEnabled) {
       logger.debug(
-        'Telemetry is disabled because neither TELEMETRY_COLLECTOR_URL nor LOGZIO_METRICS_TOKEN is defined.',
+        'Telemetry is disabled because either TELEMETRY_COLLECTOR_URL or LOGZIO_METRICS_TOKEN is undefined.',
       );
       return;
     }
