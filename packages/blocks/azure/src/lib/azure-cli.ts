f385eb782c084145cfc89150761e67135fbcccb8
BigFluffyCookie <leyla@openops.com>
2025-11-05 12:27:49 +0100

    fix lint

--- packages/blocks/azure/src/lib/azure-cli.ts
+++ packages/blocks/azure/src/lib/azure-cli.ts
@@ -18,7 +18,8 @@ export async function runCommand(
   }
 
   if (!shouldUseHostCredentials) {
-    const persistentConfigDir = process.env['AZURE_CONFIG_DIR'] ?? join(homedir(), '.azure');
+    const persistentConfigDir =
+      process.env['AZURE_CONFIG_DIR'] ?? join(homedir(), '.azure');
     envVars['AZURE_CONFIG_DIR'] = persistentConfigDir;
 
     await login(credentials, envVars);
