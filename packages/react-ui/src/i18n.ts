bf73214b920b2fe30dc62f79edbd9837c7e91c9c
Ravi Kiran <ravikiranvm@gmail.com>
2025-11-24 20:39:31 +0530

    Modify logic to load and override translation file

--- packages/react-ui/src/i18n.ts
+++ packages/react-ui/src/i18n.ts
@@ -4,20 +4,41 @@ import Backend from 'i18next-http-backend';
 import ICU from 'i18next-icu';
 import { initReactI18next } from 'react-i18next';
 
-i18n
-  .use(ICU)
-  .use(Backend)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    fallbackLng: 'en',
-    debug: false,
+const translationOverrides: Record<string, string> = {};
 
-    interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
-    },
-    returnEmptyString: false,
-    returnNull: false,
+fetch('/locales/en/translation-override.json')
+  .then((response) => (response.ok ? response.json() : {}))
+  .then((overrides) => {
+    Object.assign(translationOverrides, overrides);
+  })
+  .catch(() => {})
+  .finally(() => {
+    i18n
+      .use(ICU)
+      .use(Backend)
+      .use(LanguageDetector)
+      .use(initReactI18next)
+      .init({
+        fallbackLng: 'en',
+        debug: false,
+
+        interpolation: {
+          escapeValue: false, // not needed for react as it escapes by default
+        },
+        returnEmptyString: false,
+        returnNull: false,
+        backend: {
+          parse: (data: string) => {
+            try {
+              const parsedData = JSON.parse(data);
+              return { ...parsedData, ...translationOverrides };
+            } catch (error) {
+              console.error('Error parsing translation data', error);
+              return {};
+            }
+          },
+        },
+      });
   });
 
 export default i18n;
