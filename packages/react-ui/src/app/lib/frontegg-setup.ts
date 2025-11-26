1e616cf815f79d43ec30576217bc000e9969295c
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2025-11-26 17:43:27 +0200

    Add client id to frontegg app

--- packages/react-ui/src/app/lib/frontegg-setup.ts
+++ packages/react-ui/src/app/lib/frontegg-setup.ts
@@ -5,17 +5,38 @@ export const additionalFronteggParams = {
   prompt: 'select_account',
 };
 
-export function initializeFrontegg(url: string, tenant?: string): FronteggApp {
-  const tenantResolver = tenant ? () => ({ tenant }) : undefined;
+let fronteggApp: FronteggApp | null = null;
 
-  return initialize({
+type Options = {
+  url: string;
+  clientId?: string;
+  tenant?: string;
+};
+
+export function initializeFrontegg({
+  url,
+  clientId,
+  tenant,
+}: Options): FronteggApp {
+  if (fronteggApp) {
+    return fronteggApp;
+  }
+  const tenantResolver = tenant ? () => ({ tenant }) : undefined;
+  const options = {
     contextOptions: {
       baseUrl: url,
+      clientId: clientId,
       tenantResolver,
     },
     authOptions: {
       keepSessionAlive: true,
     },
     hostedLoginBox: true,
-  });
+  };
+  fronteggApp = initialize(options);
+  return fronteggApp;
+}
+
+export function getFronteggApp(): FronteggApp | null {
+  return fronteggApp;
 }
