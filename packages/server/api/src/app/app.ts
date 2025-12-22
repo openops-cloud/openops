4453aa628e4b4a407ffc6553728ad71d4a2e6712
Cezar Dascal <cezar@openops.com>
2025-12-22 15:00:19 +0200

    Consolidate system initialization in main startup

--- packages/server/api/src/app/app.ts
+++ packages/server/api/src/app/app.ts
@@ -193,7 +193,6 @@ export const setupApp = async (
   });
 
   app.addHook('preHandler', securityHandlerChain);
-  await systemJobsSchedule.init();
   await app.register(fileModule);
   await app.register(flagModule);
   await app.register(storeEntryModule);
