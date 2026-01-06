0eca01ceff3fddcbdeba1dff2de2f19f993e9f3d
Marcelo Gonçalves <marcelo@openops.com>
2026-01-06 14:19:41 +0000

    Add a helper function for iterating through Tables Databases (#1805)
    Part of OPS-3345.

---
+++ packages/server/api/src/app/database/seeds/tables-database-iterator.ts
@@ -0,0 +1,28 @@
+import { TablesServerContext } from '@openops/common';
+import { projectRepo } from '../../project/project-service';
+
+const listTablesDatabases = async (): Promise<TablesServerContext[]> => {
+  const projects = await projectRepo().find({
+    select: ['tablesDatabaseId', 'tablesDatabaseToken'],
+  });
+
+  return projects
+    .filter(
+      ({ tablesDatabaseId, tablesDatabaseToken }) =>
+        tablesDatabaseId != null && tablesDatabaseToken != null,
+    )
+    .map(({ tablesDatabaseId, tablesDatabaseToken }) => ({
+      tablesDatabaseId,
+      tablesDatabaseToken,
+    }));
+};
+
+export const applyToEachTablesDatabase = async (
+  run: (tablesContext: TablesServerContext) => Promise<void>,
+): Promise<void> => {
+  const tablesContexts = await listTablesDatabases();
+
+  for (const tablesContext of tablesContexts) {
+    await run(tablesContext);
+  }
+};
