e90c8cc23e62cc4094172a47f12e3e8072697e66
Marcelo Gonçalves <marcelo@openops.com>
2026-01-16 20:08:13 +0000

    WIP

--- packages/blocks/anodot/jest.config.ts
+++ packages/blocks/anodot/jest.config.ts
@@ -5,7 +5,7 @@ export default {
   testEnvironment: 'node',
   transform: {
     '^.+\\.ts$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
-  },  
+  },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../../coverage/packages/blocks/anodot',
 };
