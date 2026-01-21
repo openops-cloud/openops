771a2c7ba529a3275599eb51988139b13bb18b73
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2026-01-21 19:58:40 +0200

    fix log

--- packages/react-ui/src/app/features/connections/components/oauth2-connection-settings.tsx
+++ packages/react-ui/src/app/features/connections/components/oauth2-connection-settings.tsx
@@ -183,8 +183,8 @@ const OAuth2ConnectionSettings = ({
       pkce: authProperty.pkce ?? false,
       extraParams: authProperty.extra ?? {},
     });
-    console.log('code', code);
-    console.log('codeChallenge', codeChallenge);
+    // eslint-disable-next-line no-console
+    console.debug('received auth code', !isNil(code));
     form.setValue('request.value.code', code, { shouldValidate: true });
     form.setValue('request.value.code_challenge', codeChallenge, {
       shouldValidate: true,
