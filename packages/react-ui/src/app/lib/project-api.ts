4518e44c4c3bc02916cd0882bb47d3c71225fff1
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2026-02-12 11:03:24 +0200

    fix type

--- packages/react-ui/src/app/lib/project-api.ts
+++ packages/react-ui/src/app/lib/project-api.ts
@@ -1,21 +1,26 @@
 import { api } from '@/app/lib/api';
 import {
   ListProjectRequestForUserQueryParams,
-  Project,
+  ProjectWithoutSensitiveData,
   SeekPage,
 } from '@openops/shared';
 
 export const projectApi = {
   current: async () => {
-    return api.get<Project>('/v1/users/projects/current');
+    return api.get<ProjectWithoutSensitiveData>('/v1/users/projects/current');
   },
   list(request: ListProjectRequestForUserQueryParams) {
-    return api.get<SeekPage<Project>>('/v1/users/projects', request);
+    return api.get<SeekPage<ProjectWithoutSensitiveData>>(
+      '/v1/users/projects',
+      request,
+    );
   },
   get: async (projectId: string) => {
-    return api.get<Project>(`/v1/users/projects/${projectId}`);
+    return api.get<ProjectWithoutSensitiveData>(
+      `/v1/users/projects/${projectId}`,
+    );
   },
   getAll: async () => {
-    return api.get<Project[]>(`/v1/users/projects`);
+    return api.get<ProjectWithoutSensitiveData[]>(`/v1/users/projects`);
   },
 };
