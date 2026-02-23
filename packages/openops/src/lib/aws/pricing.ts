8f290905cf89da15a610a057f8b72360dff4eab7
Marcelo Gonçalves <marcelo@openops.com>
2026-02-23 11:10:21 +0000

    WIP

--- packages/openops/src/lib/aws/pricing.ts
+++ packages/openops/src/lib/aws/pricing.ts
@@ -10,6 +10,7 @@ import {
   Service,
 } from '@aws-sdk/client-pricing';
 import { cacheWrapper } from '@openops/server-shared';
+import { AutomaticJsonStringConversion as __AutomaticJsonStringConversion } from '@smithy/core/dist-types/submodules/serde/lazy-json';
 import { makeAwsRequest } from './aws-client-wrapper';
 import { getAwsClient } from './get-client';
 
