10ad42cb143d4be12725b60a82260577c644de00
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2026-02-25 15:55:44 +0200

    Remove Permission tooltip from OSS (#2009)
    <!--
    Ensure the title clearly reflects what was changed.
    Provide a clear and concise description of the changes made. The PR
    should only contain the changes related to the issue, and no other
    unrelated changes.
    -->
    
    Part of OPS-3675

--- packages/react-ui/src/app/common/hooks/authorization-hooks.ts
+++ packages/react-ui/src/app/common/hooks/authorization-hooks.ts
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import { authenticationSession } from '@/app/lib/authentication-session';
+import { Permission } from '@openops/shared';
 
 export const useAuthorization = () => {
   const role = authenticationSession.getUserProjectRole();
 
-  const checkAccess = React.useCallback(() => true, []);
+  const checkAccess = React.useCallback((_permission: Permission) => {
+    return true;
+  }, []);
 
   return { checkAccess, role };
 };
