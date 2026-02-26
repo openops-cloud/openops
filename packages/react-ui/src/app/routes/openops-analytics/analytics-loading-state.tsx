0882736a56ace5be943b44893fee39c356a47299
Cezar Dascal <cezar@openops.com>
2026-02-26 08:56:46 +0200

    Make loading state look the same as the native Superset loading spinner

--- packages/react-ui/src/app/routes/openops-analytics/analytics-loading-state.tsx
+++ packages/react-ui/src/app/routes/openops-analytics/analytics-loading-state.tsx
@@ -1,11 +1,11 @@
+import { LoadingSpinner } from '@openops/components/ui';
 import { t } from 'i18next';
 
 export const AnaloyticsLoadingState = () => {
   return (
-    <div className="size-full flex items-center justify-center">
-      <span className="text-muted-foreground">
-        {t('Loading dashboards...')}
-      </span>
+    <div className="size-full flex flex-col items-center justify-center gap-3">
+      <LoadingSpinner size={50} className="stroke-blueAccent-500" />
+      <span className="text-muted-foreground">{t('Loading analytics...')}</span>
     </div>
   );
 };
