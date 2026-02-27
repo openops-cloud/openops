351b7c2ac5c2301099776582f7a2e3e325307c77
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2026-02-27 13:45:29 +0200

    fix interaction tests

--- packages/ui-components/src/stories/markdown.stories.tsx
+++ packages/ui-components/src/stories/markdown.stories.tsx
@@ -220,7 +220,7 @@ aws ec2 describe-instances
     const monacoTextarea = await canvas.findByRole('textbox');
 
     expect(monacoTextarea).toBeInTheDocument();
-    expect(monacoTextarea).toHaveClass('inputarea', 'monaco-mouse-cursor-text');
+    expect(monacoTextarea).toHaveClass('native-edit-context');
 
     await waitFor(() => {
       const viewLines = canvas.getByText('aws ec2 describe-instances');
@@ -254,7 +254,7 @@ aws s3 sync\n  --exclude "*"\n  --include "*.jpg"\n  <local-dir> s3://<bucket-na
     const monacoTextarea = await canvas.findByRole('textbox');
 
     expect(monacoTextarea).toBeInTheDocument();
-    expect(monacoTextarea).toHaveClass('inputarea', 'monaco-mouse-cursor-text');
+    expect(monacoTextarea).toHaveClass('native-edit-context');
 
     await waitFor(() => {
       expect(canvas.getByText(/aws s3 sync/i)).toBeInTheDocument();
