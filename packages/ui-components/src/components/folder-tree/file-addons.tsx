c19c395b36cc6a230ec5f170c2499e48f3a96c58
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2026-03-04 14:19:03 +0200

    Permission check for navigation and folders

--- packages/ui-components/src/components/folder-tree/file-addons.tsx
+++ packages/ui-components/src/components/folder-tree/file-addons.tsx
@@ -5,11 +5,18 @@ import { FolderItem } from './types';
 type Props = {
   item: FolderItem;
   isSelected: boolean;
+  readonly: boolean;
   onViewClick: (item: FolderItem) => void;
   onEditClick: (item: FolderItem) => void;
 };
 
-const FileAddons = ({ item, isSelected, onViewClick, onEditClick }: Props) => {
+const FileAddons = ({
+  item,
+  isSelected,
+  readonly,
+  onViewClick,
+  onEditClick,
+}: Props) => {
   if (isSelected) return null;
   return (
     <div className="flex items-center gap-2">
@@ -25,17 +32,19 @@ const FileAddons = ({ item, isSelected, onViewClick, onEditClick }: Props) => {
         />
       </TooltipWrapper>
 
-      <TooltipWrapper tooltipText="Edit workflow">
-        <Pencil
-          role="button"
-          data-testid="edit-flow"
-          className="h-4 w-4 text-neutral-400 hover:text-foreground"
-          onClick={(event) => {
-            event.stopPropagation();
-            onEditClick(item);
-          }}
-        />
-      </TooltipWrapper>
+      {!readonly && (
+        <TooltipWrapper tooltipText="Edit workflow">
+          <Pencil
+            role="button"
+            data-testid="edit-flow"
+            className="h-4 w-4 text-neutral-400 hover:text-foreground"
+            onClick={(event) => {
+              event.stopPropagation();
+              onEditClick(item);
+            }}
+          />
+        </TooltipWrapper>
+      )}
     </div>
   );
 };
