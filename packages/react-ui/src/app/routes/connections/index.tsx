23f432407bc692b0eb89e294b1860d9e110b4118
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2026-03-05 16:24:39 +0200

    Add permission handling for connections page

--- packages/react-ui/src/app/routes/connections/index.tsx
+++ packages/react-ui/src/app/routes/connections/index.tsx
@@ -1,17 +1,29 @@
+import { PermissionGuard } from '@/app/common/components/permission-guard';
 import { AppConnectionsTable } from '@/app/features/connections/components/connection-table';
+import { useConnectionsContext } from '@/app/features/connections/components/connections-context';
 import { NewConnectionTypeDialog } from '@/app/features/connections/components/new-connection-type-dialog';
-import { useState } from 'react';
+import { Button, PageHeader } from '@openops/components/ui';
+import { Permission } from '@openops/shared';
+import { t } from 'i18next';
 
-export default function AppConnectionsPage() {
-  const [openNewConnectionDialog, setOpenNewConnectionDialog] = useState(false);
+export const ConnectionsHeader = () => {
+  const { setRefresh } = useConnectionsContext();
 
   return (
-    <>
-      <NewConnectionTypeDialog
-        open={openNewConnectionDialog}
-        setOpen={setOpenNewConnectionDialog}
-      ></NewConnectionTypeDialog>
-      <AppConnectionsTable></AppConnectionsTable>
-    </>
+    <PageHeader title={t('Connections')}>
+      <div className="ml-auto mr-7">
+        <NewConnectionTypeDialog
+          onConnectionCreated={() => setRefresh((prev) => !prev)}
+        >
+          <PermissionGuard permission={Permission.WRITE_APP_CONNECTION}>
+            <Button variant="default">{t('New Connection')}</Button>
+          </PermissionGuard>
+        </NewConnectionTypeDialog>
+      </div>
+    </PageHeader>
   );
+};
+
+export default function AppConnectionsPage() {
+  return <AppConnectionsTable />;
 }
