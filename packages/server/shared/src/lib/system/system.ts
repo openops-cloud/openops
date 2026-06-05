cd7e87e1160daeaf5020e878cbaa2e8dd74a1457
Marcelo Gonçalves <marcelo@openops.com>
2026-06-06 00:13:28 +0100

    WIP

--- packages/server/shared/src/lib/system/system.ts
+++ packages/server/shared/src/lib/system/system.ts
@@ -110,8 +110,8 @@ const systemPropDefaultValues: Partial<Record<SystemProp, string>> = {
   [SharedSystemProp.SMTP_ALLOWED_PORTS]: '25,465,587,2525',
   [AppSystemProp.CONNECTION_VALIDATION_CRON]: '0 5 * * *',
   [WorkerSystemProps.ENGINE_MEMORY_LIMIT_MB]: '1024',
-  [WorkerSystemProps.ENGINE_POOL_MIN_SIZE]: '3',
-  [WorkerSystemProps.ENGINE_POOL_MAX_SIZE]: '5',
+  [WorkerSystemProps.ENGINE_POOL_MIN_SIZE]: '4',
+  [WorkerSystemProps.ENGINE_POOL_MAX_SIZE]: '10',
 };
 
 export const system = {
