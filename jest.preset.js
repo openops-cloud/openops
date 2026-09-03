24a85ac8e45c603062228328c41fa469d7821022
Marcelo Gonçalves <marcelo@openops.com>
2026-09-03 17:12:03 +0100

    Update nanoid package (#2467)
    Fixes OPS-4791

--- jest.preset.js
+++ jest.preset.js
@@ -1,5 +1,14 @@
 const nxPreset = require('@nx/jest/preset').default;
 
+/**
+ * Dependencies that are published as ESM only. Jest runs on a CommonJS module
+ * registry, so these have to be handed to the transformer instead of being
+ * skipped along with the rest of node_modules.
+ */
+const esmOnlyDependencies = ['lodash-es', 'nanoid'];
+const esmOnlyDependencyGroup = esmOnlyDependencies.join('|');
+const esmOnlyDependencyPath = String.raw`node_modules[/\\](?:${esmOnlyDependencyGroup})[/\\]`;
+
 module.exports = {
   ...nxPreset,
   /* TODO: Update to latest Jest snapshotFormat
@@ -12,6 +21,9 @@ module.exports = {
    * More info: https://jestjs.io/docs/upgrading-to-jest29#snapshot-format
    */
   snapshotFormat: { escapeString: true, printBasicPrototype: true },
-  transformIgnorePatterns: ['^.+\\.js$', 'node_modules/(?!(lodash-es)/)'],
+  transformIgnorePatterns: [
+    String.raw`^(?!.*${esmOnlyDependencyPath}).+\.js$`,
+    `node_modules/(?!(${esmOnlyDependencyGroup})/)`,
+  ],
   setupFilesAfterEnv: [__dirname + '/jest.setup.js'],
 };
